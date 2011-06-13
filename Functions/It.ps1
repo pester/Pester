25f74ca12ca63c399ad54d5a76f8daa5415f048b
Martin Aatmaa <martin@aatmaa.net>
2011-06-12 23:51:48 -0700

    It fixture - fixed bug with transcripting, ensuring that stop-transcript is always called

--- Functions/It.ps1
+++ Functions/It.ps1
@@ -13,20 +13,23 @@ function It($name, [ScriptBlock] $test)
 
     Start-PesterConsoleTranscript
 
-    $test_result = & $test
+    try {
+        $test_result = & $test
 
-    if ($test_result -and $test_result.GetType().FullName -eq "PesterFailure") {
-        $failures += 1
-        $results.FailedTests += $name
-        $output | Write-Host -ForegroundColor red
-        Write-Host -ForegroundColor red $error_margin"Failure at $name in $test_file"
-        $expected = $test_result.Expected
-        $observed = $test_result.Observed
-        Write-Host -ForegroundColor red $error_margin"Expected: $expected"
-        Write-Host -ForegroundColor red $error_margin"But was : $observed"
+        if ($test_result -and $test_result.GetType().FullName -eq "PesterFailure") {
+            $failures += 1
+            $results.FailedTests += $name
+            $output | Write-Host -ForegroundColor red
+            Write-Host -ForegroundColor red $error_margin"Failure at $name in $test_file"
+            $expected = $test_result.Expected
+            $observed = $test_result.Observed
+            Write-Host -ForegroundColor red $error_margin"Expected: $expected"
+            Write-Host -ForegroundColor red $error_margin"But was : $observed"
+        }
     }
-
-    Stop-PesterConsoleTranscript
+    finally {
+        Stop-PesterConsoleTranscript
+    }
 
     if($failures -eq 0) {$output | Write-Host -ForegroundColor green;}
 }
