bfb2aa18bb10b9a1296469e076341df2d58a1cb0
Martin Aatmaa <martin@aatmaa.net>
2011-06-12 23:44:19 -0700

    It fixture - removed line by line eval since this breaks evaluating multi-line blocks

--- Functions/It.ps1
+++ Functions/It.ps1
@@ -7,33 +7,27 @@ function It($name, [ScriptBlock] $test)
 
     $output = " $margin$name"
 
-    $start_line_position = $test.StartPosition.StartLine
     $test_file = $test.File
-    $line_count = -1
     $failures = 0 
     $test_result = $true
 
     Start-PesterConsoleTranscript
 
-    foreach ( $line in $test.ToString().Split("`n;") ) {
-        $line_count++
-        $line=$line.trim()
-        if($line){
-            $test_result = Invoke-Expression $line
-            if ($test_result -and $test_result.GetType().FullName -eq "PesterFailure") {
-                $failures += 1
-                $results.FailedTests += $name
-                $output | Write-Host -ForegroundColor red
-                Write-Host -ForegroundColor red $error_margin"Failure at line: $($start_line_position + $line_count) in  $test_file"
-                Write-Host -ForegroundColor red $error_margin$error_margin$line
-                $expected = $test_result.Expected
-                $observed = $test_result.Observed
-                Write-Host -ForegroundColor red $error_margin"Expected: $expected"
-                Write-Host -ForegroundColor red $error_margin"But was : $observed"
-            }
-        }
+    $test_result = & $test
+
+    if ($test_result -and $test_result.GetType().FullName -eq "PesterFailure") {
+        $failures += 1
+        $results.FailedTests += $name
+        $output | Write-Host -ForegroundColor red
+        Write-Host -ForegroundColor red $error_margin"Failure at $name in $test_file"
+        $expected = $test_result.Expected
+        $observed = $test_result.Observed
+        Write-Host -ForegroundColor red $error_margin"Expected: $expected"
+        Write-Host -ForegroundColor red $error_margin"But was : $observed"
     }
+
     Stop-PesterConsoleTranscript
+
     if($failures -eq 0) {$output | Write-Host -ForegroundColor green;}
 }
 
