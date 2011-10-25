c2f0aaed0fae4b66813d5273e4a040ebeed9cdc8
Iristyle <ethan_j_brown@hotmail.com>
2011-10-25 15:34:21 -0300

    Added some simple + / - type checkboxes to the output stream

--- Functions/It.ps1
+++ Functions/It.ps1
@@ -16,14 +16,14 @@ function It($name, [ScriptBlock] $test)
     Start-PesterConsoleTranscript
     try{
         temp
-        $output | Write-Host -ForegroundColor green;
+        "[+] $output " | Write-Host -ForegroundColor green;
     } catch {
         $failure_message = $_.toString() -replace "Exception calling", "Assert failed on"
         $temp_line_number =  $_.InvocationInfo.ScriptLineNumber - 2
         $failure_line_number = $start_line_position + $temp_line_number
 
         $results.FailedTests += $name
-        $output | Write-Host -ForegroundColor red
+        "[-] $output" | Write-Host -ForegroundColor red
 
         Write-Host -ForegroundColor red $error_margin$failure_message
         Write-Host -ForegroundColor red $error_margin$error_margin"at line: $failure_line_number in  $test_file"
