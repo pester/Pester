fb2318108c5d0ffb730803933ed4de7fd71405f8
Ole Lynge Soerensen <olelynge@gmail.com>
2012-04-29 15:02:38 +0200

    Update example.

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,5 +1,9 @@
 nuget install pester -o packages
 $path = Resolve-Path .
-$pesterPackagePath = (gci "$path\packages").FullName
+$pesterPackage = gci "$path\packages\Pester.*"
+if ($pesterPackage.count) {
+  $pesterPackage = $pesterPackage[$pesterPackage.count-1]
+}
+$pesterPackagePath = $pesterPackage.FullName
 Import-Module "$pesterPackagePath\tools\Pester.psm1"
 Invoke-Pester "$path\src"
