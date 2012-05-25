bd273735ae88a2a60557374c5d69a2271add0796
Matt Wrock <matt@mattwrock.com>
2012-05-24 21:24:46 -0700

    put back enableExit

--- Pester.psm1
+++ Pester.psm1
@@ -2,7 +2,7 @@ Resolve-Path $PSScriptRoot\Functions\*.ps1 |
     ? { -not ($_.ProviderPath.Contains(".Tests.")) } |
     % { . $_.ProviderPath }
 
-function Invoke-Pester($relative_path = ".", $test_name = $null) {
+function Invoke-Pester($relative_path = ".", $test_name = $null, [switch] $EnableExit) {
 
     Reset-GlobalTestResults
     . "$PSScriptRoot\ObjectAdaptations\PesterFailure.ps1"
