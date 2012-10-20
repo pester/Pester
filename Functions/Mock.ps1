f4e69b2c5b860db716e06482954562ea89367e27
Matt Wrock <matt@mattwrock.com>
2012-10-19 21:45:35 -0700

    add documentation for Assert-MockCalled

--- Functions/Mock.ps1
+++ Functions/Mock.ps1
@@ -104,8 +104,7 @@ param(
     [ScriptBlock]$parameterFilter = {$True}    
 )
 
-    $origCommand = (Microsoft.PowerShell.Core\Get-Command $commandName -ErrorAction SilentlyContinue)
-    if(!$origCommand){ Throw "Could not find Command $commandName"}
+    $origCommand = Validate-Command
     $filterTest=&($parameterFilter)
     if($filterTest -ne $True -and $filterTest -ne $False){ throw "The Parameter Filter must return a boolean"}
     $blocks = @{Mock=$mockWith; Filter=$parameterFilter; Verifiable=$verifiable}
@@ -172,6 +171,66 @@ This will not throw an exception because the mock was invoked.
 }
 
 function Assert-MockCalled {
+<#
+.SYNOPSIS
+Checks if a Mocked command has been called a certain number of times 
+and throws an exception if it has not.
+
+.DESCRIPTION
+This command checks the call history of the specified Command since 
+the Mock was declared. If it had been called less than the number of 
+times specified (1 is the default), then an exception is thrown. You 
+may specify 0 times if you want to make sure that the mock has NOT 
+been called. If you include the Exactly switch, the number of times 
+that the command has been called must mach exactly with the number of 
+times specified on this command.
+
+.EXAMPLE
+C:\PS>Mock Set-Content {}
+
+{... Some Code ...}
+
+C:\PS>Assert-MockCalled Set-Content
+
+This will throw an exception and cause the test to fail if Set-Content is not called in Some Code.
+
+.EXAMPLE
+C:\PS>Mock Set-Content -parameterFilter {$path.StartsWith("$env:temp\")}
+
+{... Some Code ...}
+
+C:\PS>Assert-MockCalled Set-Content 2 {$path=$env:temp\test.txt}
+
+This will throw an exception if some code calls Set-Content on $path=$env:temp\test.txt less than 2 times 
+
+.EXAMPLE
+C:\PS>Mock Set-Content {}
+
+{... Some Code ...}
+
+C:\PS>Assert-MockCalled Set-Content 0
+
+This will throw an exception if some code calls Set-Content at all
+
+.EXAMPLE
+C:\PS>Mock Set-Content {}
+
+{... Some Code ...}
+
+C:\PS>Assert-MockCalled Set-Content -Exactly 2
+
+This will throw an exception if some code does not call Set-Content Exactly two times.
+
+.NOTES
+While Mock will only mock commands if the Parameter Filter
+matches the Parameters passed to the command, Assert-MockCalled 
+will count calls to a command whether they are mocked or not. 
+The Command must be declared as a mock but the parameter 
+filter in its mock declaration do not need to include the 
+parameter Filter specified by Assert-MockCalled.
+
+#>
+
 param(
     [string]$commandName,
     [switch]$Exactly,
@@ -179,6 +238,7 @@ param(
     [ScriptBlock]$parameterFilter = {$True}    
 )
     $mock = $global:mockTable.$commandName
+    if(!$mock) { Throw "You did not declare a mock of the $commandName Command."}
     Microsoft.PowerShell.Management\Set-Item Function:\Pester_TempParamTest -value "$($mock.CmdLet) `r`n param ( $($mock.Params) ) `r`n$parameterFilter"
     $cmd=(Microsoft.PowerShell.Core\Get-Command Pester_TempParamTest)
     $qualifiedCalls = ($global:mockCallHistory | ? {$_.CommandName -eq $commandName} | ? {$p=$_.params;&($cmd) @p} )
@@ -197,6 +257,12 @@ function Clear-Mocks {
         if($mockCallHistory) {$mockCallHistory.Clear()}
         Get-ChildItem Function: | ? { $_.Name.StartsWith("PesterIsMocking_") } | % {Rename-Item Function:\$_ "script:$($_.Name.Replace('PesterIsMocking_', ''))"}
     }
+}
+
+function Validate-Command([string]$commandName) {
+    $origCommand = (Microsoft.PowerShell.Core\Get-Command $commandName -ErrorAction SilentlyContinue)
+    if(!$origCommand){ Throw "Could not find Command $commandName"}
+    return $origCommand
 }
 
 function MockPrototype {
