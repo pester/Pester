cf04a142d424599a95425fb81b0c40f70580c1db
Scott Muc <scottmuc@gmail.com>
2012-12-29 02:58:31 -0800

    renaming default.ps1 to a more clarifying name build.psake.ps1

--- Build.bat
+++ Build.bat
@@ -2,13 +2,14 @@
 
 if '%1'=='/?' goto help
 if '%1'=='-help' goto help
+if '%1'=='help' goto help
 if '%1'=='-h' goto help
 
 %~dp0vendor\tools\nuget.exe Install %~dp0vendor\packages.config -o %~dp0vendor\packages
 powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-  "$psakeDir = ([array](dir %~dp0Vendor\packages\psake.*))[-1]; .$psakeDir\tools\psake.ps1 %* -ScriptPath $psakeDir\tools ; if ($psake.build_success -eq $false) { exit 1 } else { exit 0 }"
+  "$psakeDir = ([array](dir %~dp0Vendor\packages\psake.*))[-1]; .$psakeDir\tools\psake.ps1 build.psake.ps1 %* -ScriptPath $psakeDir\tools ; if ($psake.build_success -eq $false) { exit 1 } else { exit 0 }"
 goto :eof
 
 :help
 powershell -NoProfile -ExecutionPolicy Bypass -Command ^
-  "$psakeDir = ([array](dir %~dp0Vendor\packages\psake.*))[-1]; .$psakeDir\tools\psake.ps1 -ScriptPath $psakeDir\tools -help"
+  "$psakeDir = ([array](dir %~dp0Vendor\packages\psake.*))[-1]; .$psakeDir\tools\psake.ps1 build.psake.ps1 -docs -ScriptPath $psakeDir\tools"
