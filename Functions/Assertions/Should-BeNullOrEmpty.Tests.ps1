3d3f2feb1639c9414cc5f68ad77a285d4e66a94a
Paul Stack <public@paulstack.co.uk>
2013-01-26 22:48:14 -0800

    Update Functions/Assertions/Should-BeNullOrEmpty.Tests.ps1
    Added Contexts to group the test types for clarity

--- Functions/Assertions/Should-BeNullOrEmpty.Tests.ps1
+++ Functions/Assertions/Should-BeNullOrEmpty.Tests.ps1
@@ -14,20 +14,8 @@ function Should-BeNullOrEmpty {
 
 Describe "Should-BeNullOrEmpty" {
 
-  It "throws a PesterFailure when the variable has a string value" {
-      $string = "this is a non empty string"
-
-      try {
-       Should-BeNullOrEmpty $string
-        $failure_thrown = $false
-      } catch {
-        $failure_thrown = $true
-      }
-
-      $failure_thrown.should.be($true)
-    }
-
-    It "throws a PesterFailure when the variable is an array with an element" {
+Context("when dealing with an array") {
+ It "throws a PesterFailure when the variable is an array with an element" {
       $nonNullArray = @("Element1")
 
       try {
@@ -38,19 +26,6 @@ Describe "Should-BeNullOrEmpty" {
       }
 
       $failure_thrown.should.be($true)
-    }
-
-    It "passes the test when the string is empty" {
-        $string = ""
-
-      try {
-       Should-BeNullOrEmpty $string
-        $failure_thrown = $false
-      } catch {
-        $failure_thrown = $true
-      }
-
-      $failure_thrown.should.be($false)
     }
 
     It "passes the test when the array is empty" {
@@ -65,4 +40,41 @@ Describe "Should-BeNullOrEmpty" {
 
       $failure_thrown.should.be($false)
     }
+
+}
+
+Context("when dealing with strings") {
+
+It "throws a PesterFailure when the variable has a string value" {
+      $string = "this is a non empty string"
+
+      try {
+       Should-BeNullOrEmpty $string
+        $failure_thrown = $false
+      } catch {
+        $failure_thrown = $true
+      }
+
+      $failure_thrown.should.be($true)
+    }
+
+   
+
+    It "passes the test when the string is empty" {
+        $string = ""
+
+      try {
+       Should-BeNullOrEmpty $string
+        $failure_thrown = $false
+      } catch {
+        $failure_thrown = $true
+      }
+
+      $failure_thrown.should.be($false)
+    }
+}
+
+  
+
+    
 }
