70a623d739d63bff6b6cadeca0272dc7a13b4755
Scott Muc <scottmuc@gmail.com>
2013-01-28 09:09:58 -0800

    trying out a new assertion syntax

--- Functions/Assertions/Should.Tests.ps1
+++ Functions/Assertions/Should.Tests.ps1
@@ -1,51 +1,44 @@
 
+function PesterEqual($expected, $value) {
+  return ($expected -eq $value)
+}
+
+function PesterNotEqual($expected, $value) {
+  return ($expected -ne $value)
+}
+
 function Should {
 
   [CmdletBinding()]
   param(
     [Parameter(ValueFromPipeline=$TRUE)]
     $value,
-    [alias("Equal")]
+    [Parameter(Position=0,Mandatory=1)]
+    $testMethod,
+    [Parameter(Position=1,Mandatory=1)]
     $expected_value
   )
 
-  $equal = ($value -eq $expected_value)
+  $pesterTestMethod = "Pester$testMethod"
+  $testFailed = -not (Invoke-Expression -Command "$pesterTestMethod $expected_value $value")
 
-  if (-not $equal) {
+  if ($testFailed) {
     throw New-Object PesterFailure($expected_value, $value)
   }
 }
-
-function Should-Not {
-
-  [CmdletBinding()]
-  param(
-    [Parameter(ValueFromPipeline=$TRUE)]
-    $value,
-    [alias("Equal")]
-    $unexpected_value
-  )
-
-  $equal = ($value -eq $unexpected_value)
-
-  if ($equal) {
-    throw New-Object PesterFailure($unexpected_value, $value)
-  }
-}
-
 
 Describe "Should" {
 
   Context("when comparing equal values") {
     It "does not return any errors" {
-      1 | Should -Equal 1
+      1 | Should Equal 1
     }
   }
 
   Context("when comparing unueqal values") {
     It "throws a PesterFailure" {
       try {
-        2 | Should -Equal 1
+        2 | Should Equal 1
         $failure_thrown = $false
       } catch {
         $failure_thrown = $true
@@ -60,14 +53,14 @@ Describe "Should-Not" {
 
   Context("when comparing unequal values") {
     It "does not return any errors" {
-      2 | Should-Not -Equal 1
+      2 | Should NotEqual 1
     }
   }
 
   Context("when comparing equal values") {
     It "throws a PesterFailure" {
       try {
-        1 | Should-Not -Equal 1
+        1 | Should NotEqual 1
         $failure_thrown = $false
       } catch {
         $failure_thrown = $true
