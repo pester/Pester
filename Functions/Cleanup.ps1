a7c480e0a833833b889e93760a43b329ac35d93b
Johan Leino <leino@outlook.com>
2013-09-05 14:02:37 +0200

    Need to explicitly call remove-item (since it can be mocked during cleanup)

--- Functions/Cleanup.ps1
+++ Functions/Cleanup.ps1
@@ -1,7 +1,7 @@
 function Cleanup {
     Clear-Mocks
 	if (Test-Path $TestDrive) {
-        Remove-Item $TestDrive -Recurse -Force
+        Microsoft.PowerShell.Management\Remove-Item $TestDrive -Recurse -Force
         Remove-PSDrive -Name TestDrive -Scope Global -Force
     }    
 }
