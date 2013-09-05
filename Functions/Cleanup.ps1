06587cf8ae95723387b3388ec9c9cf967ab069db
Johan Leino <johan@leino.se>
2013-09-05 09:23:00 +0200

    My bad...was just wrapping In outside of Context
    will add that to the wiki as an example

--- Functions/Cleanup.ps1
+++ Functions/Cleanup.ps1
@@ -1,7 +1,7 @@
 function Cleanup {
     Clear-Mocks
 	if (Test-Path $TestDrive) {
-		Remove-Item $TestDrive -Recurse -Force -ErrorAction:SilentlyContinue
-        Remove-PSDrive -Name TestDrive -Scope Global -Force -ErrorAction:SilentlyContinue
+        Remove-Item $TestDrive -Recurse -Force
+        Remove-PSDrive -Name TestDrive -Scope Global -Force
     }    
 }
