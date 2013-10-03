cdfad335db9adb945a7dc07e451b29c5f92be2fd
edharper01 <edwardharper01@gmail.com>
2013-10-03 16:46:10 +0100

    Fix for Issue #58 - thrown error is reported, and can be compared to an expected value

--- Functions/Assertions/PesterThrow.Tests.ps1
+++ Functions/Assertions/PesterThrow.Tests.ps1
@@ -12,5 +12,17 @@ Describe "PesterThrow" {
     It "returns false if the statement does not throw an exception" {
         Test-NegativeAssertion (PesterThrow { 1 + 1 })
     }
+    
+    It "returns true if the statement throws an exception and the actual error text matches the expected error text" {
+        Test-PositiveAssertion (PesterThrow { throw "expected error"} "expected error")
+    }    
+
+    It "returns true if the statement throws an exception and the actual error text matches the expected error pattern" {
+        Test-PositiveAssertion (PesterThrow { throw "expected error"} "expected*")
+    }     
+    
+    It "returns false if the statement throws an exception but the actual error text does not match the expected error text" {
+        Test-NegativeAssertion (PesterThrow { throw "expected error"} "will not match")
+    }        
 }
 
--- Functions/Assertions/PesterThrow.ps1
+++ Functions/Assertions/PesterThrow.ps1
@@ -1,24 +1,38 @@
 
 # because this is a script block, the user will have to
 # wrap the code they want to assert on in { }
-function PesterThrow([scriptblock] $script) {
+function PesterThrow([scriptblock] $script, $expectedErrorMessage) {
     $itThrew = $false
     try {
         # Piping to Out-Null so results of script exeution
         # does not remain on the pipeline
         & $script | Out-Null
     } catch {
-        $itThrew = $true
+
+        $pester.ActualExceptionMessage = $_.Exception.Message
+        if (($expectedErrorMessage) -and $pester.ActualExceptionMessage -cnotlike $expectedErrorMessage) {
+            $itThrew = $false
+        } else {
+            $itThrew = $true
+        }
     }
 
     return $itThrew
 }
 
-function PesterThrowFailureMessage($expected, $value) {
-    return "Expected: the expression to throw an exception"
+function PesterThrowFailureMessage($value, $expected) {
+    if ($expected) {
+      return "Expected: the expression to throw an exception with message {0}, But message was {1}" -f $expected, $pester.ActualExceptionMessage
+    } else {
+      return "Expected: the expression to throw an exception"
+    }
 }
 
-function NotPesterThrowFailureMessage($expected, $value) {
-    return "Expected: the expression to not throw an exception"
+function NotPesterThrowFailureMessage($value, $expected) {
+    if ($expected) {
+        return "Expected: the expression to not throw an exception with message {0}, But message was {1}" -f $expected, $pester.ActualExceptionMessage
+    } else {
+        return "Expected: the expression to not throw an exception. Message was {0}" -f $pester.ActualExceptionMessage
+    }
 }
 
