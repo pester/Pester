c10cb962c38d66610a8cab61a4dda4c8e06b9d74
Scott Muc <scottmuc@gmail.com>
2013-10-13 19:15:34 -0700

    rejigged tests to handle error code matching fixes #58
    * This new behavior sort of affected previous tests. Fixed those up and
    added a helper function to cleanup the PesterThrow function.

--- Functions/Assertions/PesterThrow.Tests.ps1
+++ Functions/Assertions/PesterThrow.Tests.ps1
@@ -12,21 +12,51 @@ Describe "PesterThrow" {
     It "returns false if the statement does not throw an exception" {
         Test-NegativeAssertion (PesterThrow { 1 + 1 })
     }
-    
-    It "returns true if the statement throws an exception and the actual error text matches the expected error text" {
-        Test-PositiveAssertion (PesterThrow { throw "expected error"} "expected error")
-    }    
 
-    It "returns false if the statement throws an exception and the actual error does not match the case of the expected error text" {
-        Test-NegativeAssertion (PesterThrow { throw "expected error"} "EXPECTED ERROR")
-    }     
-    
+    It "returns true if the statement throws an exception and the actual error text matches the expected error text" {
+        $expectedErrorMessage = "expected error message"
+        Test-PositiveAssertion (PesterThrow { throw $expectedErrorMessage } $expectedErrorMessage)
+    }
+
+    It "returns false if the statement throws an exception and the actual error does not match the expected error text" {
+        $unexpectedErrorMessage = "unexpected error message"
+        $expectedErrorMessage = "some expected error message"
+        Test-NegativeAssertion (PesterThrow { throw $unexpectedErrorMessage} $expectedErrorMessage)
+    }
+
     It "returns true if the statement throws an exception and the actual error text matches the expected error pattern" {
-        Test-PositiveAssertion (PesterThrow { throw "expected error"} "expected*")
-    }     
-    
-    It "returns false if the statement throws an exception but the actual error text does not match the expected error text" {
-        Test-NegativeAssertion (PesterThrow { throw "expected error"} "will not match")
-    }        
+        Test-PositiveAssertion (PesterThrow { throw "expected error"} "error")
+    }
+
+}
+
+Describe "Get-DoMessagesMatch" {
+
+    It "returns true if the actual message is the same as the expected message" {
+        $expectedErrorMessage = "expected"
+        $actualErrorMesage = "expected"
+        $result = Get-DoMessagesMatch $actualErrorMesage $expectedErrorMessage
+        $result | Should Be $True
+    }
+
+    It "returns false if the actual message is not the same as the expected message" {
+        $expectedErrorMessage = "some expected message"
+        $actualErrorMesage = "unexpected"
+        $result = Get-DoMessagesMatch $actualErrorMesage $expectedErrorMessage
+        $result | Should Be $False
+    }
+
+    It "returns false is there's no expectation" {
+        $result = Get-DoMessagesMatch "" ""
+        $result | Should Be $False
+    }
+
+    It "returns true if the expected error is contained in the actual message" {
+        $actualErrorMesage = "this is a long error message"
+        $expectedText = "long error"
+        $result = Get-DoMessagesMatch $actualErrorMesage $expectedText
+        $result | Should Be $True
+    }
+
 }
 
--- Functions/Assertions/PesterThrow.ps1
+++ Functions/Assertions/PesterThrow.ps1
@@ -2,9 +2,9 @@
 # because this is a script block, the user will have to
 # wrap the code they want to assert on in { }
 function PesterThrow([scriptblock] $script, $expectedErrorMessage) {
-    $itThrew = $false
-	$pester.ActualExceptionMessage = $null
-  $pester.ActualExceptionWasThrown = $false
+    $pester.ActualExceptionMessage = ""
+    $pester.ActualExceptionWasThrown = $false
+
     try {
         # Piping to Out-Null so results of script exeution
         # does not remain on the pipeline
@@ -12,19 +12,22 @@ function PesterThrow([scriptblock] $script, $expectedErrorMessage) {
     } catch {
         $pester.ActualExceptionWasThrown = $true
         $pester.ActualExceptionMessage = $_.Exception.Message
-        if (($expectedErrorMessage) -and $pester.ActualExceptionMessage -cnotlike $expectedErrorMessage) {
-            $itThrew = $false
-        } else {
-            $itThrew = $true
-        }
     }
 
-    return $itThrew
+    if ($pester.ActualExceptionWasThrown) {
+        return (Get-DoMessagesMatch $pester.ActualExceptionMessage $expectedErrorMessage)
+    }
+    return $false
+}
+
+function Get-DoMessagesMatch($value, $expected) {
+    if ($expected -eq "") { return $false }
+    return $value.Contains($expected)
 }
 
 function PesterThrowFailureMessage($value, $expected) {
     if ($expected) {
-      return "Expected: the expression to throw an exception with message {{{0}}}, an exception was {2}raised, message was {{{1}}}" -f 
+      return "Expected: the expression to throw an exception with message {{{0}}}, an exception was {2}raised, message was {{{1}}}" -f
               $expected, $pester.ActualExceptionMessage,(@{$true="";$false="not "}[$pester.ActualExceptionWasThrown])
     } else {
       return "Expected: the expression to throw an exception"
@@ -33,7 +36,7 @@ function PesterThrowFailureMessage($value, $expected) {
 
 function NotPesterThrowFailureMessage($value, $expected) {
     if ($expected) {
-        return "Expected: the expression to not throw an exception with message {{{0}}}, an exception was {2}raised, message was {{{1}}}" -f 
+        return "Expected: the expression to not throw an exception with message {{{0}}}, an exception was {2}raised, message was {{{1}}}" -f
               $expected, $pester.ActualExceptionMessage,(@{$true="";$false="not "}[$pester.ActualExceptionWasThrown])
     } else {
         return "Expected: the expression to not throw an exception. Message was {{{0}}}" -f $pester.ActualExceptionMessage
