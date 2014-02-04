a77ce268feb27d0914be8e3798bf7ed85295fd84
Chris Hunt <cdhunt@gmail.com>
2014-02-04 08:32:48 -0500

    Send output for failed tests to warning stream

--- Functions/It.ps1
+++ Functions/It.ps1
@@ -106,14 +106,12 @@ function write-PesterResult{
     $pester.output = " $($pester.margin)$name"
     $pester.humanSeconds = Get-HumanTime $pester.testTime.TotalSeconds
     if($pester.testResult.success) {
-        Write-Verbose "[+] $($pester.output) $($pester.humanSeconds)"# | Write-Host -ForegroundColor DarkGreen;
+        Write-Verbose "[+] $($pester.output) $($pester.humanSeconds)"
     }
     else {
-        Write-Verbose "[-] $($pester.output) $($pester.humanSeconds)"# | Write-Host -ForegroundColor red
-        Write-Verbose ("{0}{1}" -f $pester.error_margin, $pester.testResult.failureMessage)
-        Write-Verbose ("{0}{1}" -f $pester.error_margin, $pester.testResult.stackTrace)
-        # Write-Host -ForegroundColor red $($pester.error_margin)$($pester.testResult.failureMessage)
-        # Write-Host -ForegroundColor red $($pester.error_margin)$($pester.testResult.stackTrace)
+        Write-Warning "[-] $($pester.output) $($pester.humanSeconds)"
+        Write-Warning ("{0}{1}" -f $pester.error_margin, $pester.testResult.failureMessage)
+        Write-Warning ("{0}{1}" -f $pester.error_margin, $pester.testResult.stackTrace)
     }
 }
 
