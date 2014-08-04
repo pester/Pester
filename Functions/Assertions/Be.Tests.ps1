fbcc5561a6eef337ae4e3b32d4b01af76665c0a6
Dave Wyatt <dlwyatt115@gmail.com>
2014-08-04 11:18:18 -0400

    More test updates
    Testing case-sensitive behavior of Be / BeExactly on elements in arrays.

--- Functions/Assertions/Be.Tests.ps1
+++ Functions/Assertions/Be.Tests.ps1
@@ -5,6 +5,7 @@ InModuleScope Pester {
         It "returns true if the 2 arguments are equal" {
             Test-PositiveAssertion (PesterBe 1 1)
         }
+        
         It "returns true if the 2 arguments are equal and have different case" {
             Test-PositiveAssertion (PesterBe "A" "a")
         }
@@ -17,15 +18,25 @@ InModuleScope Pester {
             $array = @(1,2,3,4,'I am a string', (New-Object psobject -Property @{ IAm = 'An Object' }))
             $array | Should Be $array
         }
+
+        It 'Compares arrays with correct case-insensitive behavior' {
+            $string = 'I am a string'
+            $array = @(1,2,3,4,$string)
+            $arrayWithCaps = @(1,2,3,4,$string.ToUpper())
+
+            $array | Should Be $arrayWithCaps
+        }
     }
 
     Describe "BeExactly" {
         It "passes if letter case matches" {
             'a' | Should BeExactly 'a'
         }
+        
         It "fails if letter case doesn't match" {
             'A' | Should Not BeExactly 'a'
         }
+
         It "passes for numbers" {
             1 | Should BeExactly 1
             2.15 | Should BeExactly 2.15
@@ -34,6 +45,14 @@ InModuleScope Pester {
         It 'Compares Arrays properly' {
             $array = @(1,2,3,4,'I am a string', (New-Object psobject -Property @{ IAm = 'An Object' }))
             $array | Should BeExactly $array
+        }
+
+        It 'Compares arrays with correct case-sensitive behavior' {
+            $string = 'I am a string'
+            $array = @(1,2,3,4,$string)
+            $arrayWithCaps = @(1,2,3,4,$string.ToUpper())
+
+            $array | Should Not BeExactly $arrayWithCaps
         }
     }
 }
