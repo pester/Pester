16ccc69f59915f03e7a039f748eb55d8c0eb77bf
Dave Wyatt <dlwyatt115@gmail.com>
2014-08-04 10:58:04 -0400

    More Should Be and BeExactly updates
    Moved these assertions into the same file, which makes sense for organization when they use the same helper functions.
    
    Be / BeExactly now make use of the Compare-Object cmdlet, which has demonstrated more consistent behavior when arrays containing other arrays are passed to Should (though I can't imagine users actually need to do that, at least we handle it properly.)  It may also be easier to explain the behavior of "Should Be" in terms of it being a wrapper around Compare-Object.
    
    There were a couple of tests which relied on the behavior of the eq operator in the old implementation of Should Be (automatic casting of operands).  These tests have been updated to use Should Not Be $null instead of Should Be $true (where a non-boolean value is piped in.)

--- Functions/Assertions/Be.ps1
+++ Functions/Assertions/Be.ps1
@@ -1,16 +1,6 @@
 
 function PesterBe($value, $expected) {
-    $expectedArray = @($expected)
-    $valueArray = @($value)
-    
-    if ($expectedArray.Count -ne $valueArray.Count) { return $false }
-    
-    for ($i = 0; $i -lt $expectedArray.Count; $i++)
-    {
-        if ($expectedArray[$i] -ne $valueArray[$i]) { return $false }
-    }
-    
-    return $true
+    return CompareArrays @($value) @($expected)
 }
 
 function PesterBeFailureMessage($value, $expected) {
@@ -21,3 +11,63 @@ function NotPesterBeFailureMessage($value, $expected) {
     return "Expected: value was {$value}, but should not have been the same"
 }
 
+function PesterBeExactly($value, $expected) {
+    return CompareArrays @($value) @($expected) -CaseSensitive
+}
+
+function PesterBeExactlyFailureMessage($value, $expected) {
+    return "Expected: exactly {$expected}, But was {$value}"
+}
+
+function NotPesterBeExactlyFailureMessage($value, $expected) {
+    return "Expected: value was {$value}, but should not have been exactly the same"
+}
+
+function CompareArrays
+{
+    param (
+        [object[]] $First,
+        [object[]] $Second,
+        [switch] $CaseSensitive
+    )
+
+    $params = @{ SyncWindow = 0 }
+    if ($CaseSensitive)
+    {
+        $params['CaseSensitive'] = $true
+    }
+
+    $placeholderForNull = New-Object object
+
+    $First = @(ReplaceValueInArray -Array $First -Value $null -NewValue $placeholderForNull)
+    $Second = @(ReplaceValueInArray -Array $Second -Value $null -NewValue $placeholderForNull) 
+
+    $arraysAreEqual = ($null -eq (Compare-Object $First $Second @params))
+
+    return $arraysAreEqual
+}
+
+function ReplaceValueInArray
+{
+    param (
+        [object[]] $Array,
+        [object] $Value,
+        [object] $NewValue
+    )
+
+    foreach ($object in $Array)
+    {
+        if ($Value -eq $object)
+        {
+            $NewValue
+        }
+        elseif (@($object).Count -gt 1)
+        {
+            ReplaceValueInArray -Array @($object) -Value $Value -NewValue $NewValue
+        }
+        else
+        {
+            $object
+        }
+    }
+}
