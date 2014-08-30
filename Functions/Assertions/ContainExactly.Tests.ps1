603209dfd4cbc121c26f811bfc0a2bfef5eed490
Joel Bennett <Jaykul@HuddledMasses.org>
2014-08-30 03:36:38 -0400

    Tweak Match and MatchExactly so they can be used for regex on files Fix Should Contain and Should ContainExactly * You can specify more than one line in the pattern * RegEx escaped so that it is actually "contains" instead of "match" * Can also use contains on strings now

--- Functions/Assertions/ContainExactly.Tests.ps1
+++ Functions/Assertions/ContainExactly.Tests.ps1
@@ -3,13 +3,25 @@ Set-StrictMode -Version Latest
 InModuleScope Pester {
     Describe "PesterContainExactly" {
         Context "when testing file contents" {
-            Setup -File "test.txt" "this is line 1`nPester is awesome"
+            Setup -File "test.txt" "Is this line 1?`nPester is awesome"
             It "returns true if the file contains the specified content exactly" {
                 Test-PositiveAssertion (PesterContainExactly "$TestDrive\test.txt" "Pester")
             }
 
             It "returns false if the file does not contain the specified content exactly" {
                 Test-NegativeAssertion (PesterContainExactly "$TestDrive\test.txt" "pESTER")
+            }
+
+            It "works with more than one line" {
+                Test-PositiveAssertion (PesterContainExactly "$TestDrive\test.txt" "line 1?`nPester")
+            }
+
+            It "escapes regular expression characters" {
+                Test-PositiveAssertion (PesterContainExactly "$TestDrive\test.txt" "1?")
+            }
+
+            It "works on strings too, not just files" {
+                Test-PositiveAssertion (PesterContainExactly "This is a test`nIsn't it awesome?" "awesome?")
             }
         }
     }
--- Functions/Assertions/Match.ps1
+++ Functions/Assertions/Match.ps1
@@ -1,6 +1,11 @@
 
 function PesterMatch($value, $expectedMatch) {
-    return ($value -match $expectedMatch)
+    $ofs = "`n"
+    if($value -isnot [string] -and $value -isnot [string[]] -and (Test-Path $value -ErrorAction SilentlyContinue)) {
+        return "$(Get-Content $value)" -match $expectedMatch
+    } else {
+        return "$value" -match $expectedMatch
+    }
 }
 
 function PesterMatchFailureMessage($value, $expectedMatch) {
