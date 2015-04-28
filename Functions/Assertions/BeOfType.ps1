9e634bc3c439f9d92f13c591fa15d43135b8764a
Joel Bennett <Jaykul@HuddledMasses.org>
2015-04-28 02:16:46 -0400

    Rename BeA to BeOfType

--- Functions/Assertions/BeOfType.ps1
+++ Functions/Assertions/BeOfType.ps1
@@ -1,5 +1,5 @@
 
-function PesterBeA($value, $expectedType) {
+function PesterBeOfType($value, $expectedType) {
     trap [System.Management.Automation.PSInvalidCastException] { return $false }
     if($expectedType -is [string] -and !($expectedType -as [Type])) {
         $expectedType = $expectedType -replace '^\[(.*)\]$','$1'
@@ -7,25 +7,25 @@ function PesterBeA($value, $expectedType) {
     return [bool]($value -is $expectedType)
 }
 
-function PesterBeAFailureMessage($value, $expectedType) {
+function PesterBeOfTypeFailureMessage($value, $expectedType) {
     if($expectedType -is [string] -and !($expectedType -as [Type])) {
         $expectedType = $expectedType -replace '^\[(.*)\]$','$1'
     }
     if($Type = $expectedType -as [type]) {
-        return "Expected: ${value} to be a [$Type]"
+        return "Expected: ${value} to be of type [$Type]"
     } else {
-        return "Expected: ${value} to be a [$expectedType], but unable to find type [$expectedType]. Make sure that the assembly that contains that type is loaded."
+        return "Expected: ${value} to be of type [$expectedType], but unable to find type [$expectedType]. Make sure that the assembly that contains that type is loaded."
     }
 }
 
-function NotPesterBeAFailureMessage($value, $expectedType) {
+function NotPesterBeOfTypeFailureMessage($value, $expectedType) {
     if($expectedType -is [string] -and -not $expectedType -as [Type]) {
         $expectedType = $expectedType -replace '^\[(.*)\]$','$1'
     }
     if($Type = $expectedType -as [type]) {
-        return "Expected: {$value} to be anything but a ${Type}, but it's a ${Type}"
+        return "Expected: {$value} to be of any type except [${Type}], but it's a [${Type}]"
     } else {
-        return "Expected: ${value} to be anything but a [$expectedType], but unable to find type [$expectedType]. Make sure that the assembly that contains that type is loaded."
+        return "Expected: ${value} to be of any type except [$expectedType], but unable to find type [$expectedType]. Make sure that the assembly that contains that type is loaded."
     }    
 }
 
