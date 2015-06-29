ac24edfc54f27c223556f5c0f405184d7c056271
Kirill Teplinskiy <tkirill128@gmail.com>
2015-06-29 20:56:23 +0500

    Fix Unicode issue in `Contain` and `ContainExactly`.
    fix #377

--- Functions/Assertions/Contain.Tests.ps1
+++ Functions/Assertions/Contain.Tests.ps1
@@ -1,18 +1,24 @@
-Set-StrictMode -Version Latest
+﻿Set-StrictMode -Version Latest
 
 InModuleScope Pester {
     Describe "PesterContain" {
         Context "when testing file contents" {
-            Setup -File "test.txt" "this is line 1`nrush is awesome"
+            Setup -File "test.txt" "this is line 1`nrush is awesome`nAnd this is Unicode: ☺"
+
             It "returns true if the file contains the specified content" {
                 Test-PositiveAssertion (PesterContain "$TestDrive\test.txt" "rush")
             }
+
             It "returns true if the file contains the specified content with different case" {
                 Test-PositiveAssertion (PesterContain "$TestDrive\test.txt" "RUSH")
             }
 
             It "returns false if the file does not contain the specified content" {
                 Test-NegativeAssertion (PesterContain "$TestDrive\test.txt" "slime")
+            }
+
+            It "returns true if the file contains the specified UTF8 content" {
+                Test-PositiveAssertion (PesterContain "$TestDrive\test.txt" "☺")
             }
         }
     }
