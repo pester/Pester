9aa6f699ba271a26c8c8a1a5ddc25cbae615fac3
Dave Wyatt <dlwyatt115@gmail.com>
2016-06-05 11:36:03 -0400

    Fixing whitespace

--- Functions/Describe.ps1
+++ Functions/Describe.ps1
@@ -82,18 +82,18 @@ function DescribeImpl {
     param(
         [Parameter(Mandatory = $true, Position = 0)]
         [string] $Name,
-        
+
         [Alias('Tags')]
         $Tag=@(),
-        
+
         [Parameter(Position = 1)]
         [ValidateNotNull()]
         [ScriptBlock] $Fixture = $(Throw "No test script block is provided. (Have you put the open curly brace on the next line?)"),
 
         $Pester,
-        
+
         [scriptblock] $DescribeOutputBlock,
-        
+
         [scriptblock] $TestOutputBlock
     )
 
