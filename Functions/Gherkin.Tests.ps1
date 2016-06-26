2d0edd87a310d03c6c5a23603780b5a43535dc72
Dave Wyatt <dlwyatt115@gmail.com>
2016-06-26 10:08:14 -0400

    Tweaking gherkin tests to run in background job

--- Functions/Gherkin.Tests.ps1
+++ Functions/Gherkin.Tests.ps1
@@ -1,48 +1,52 @@
 Set-StrictMode -Version Latest
 $scriptRoot = Split-Path (Split-Path $MyInvocation.MyCommand.Path)
 
-# I don't know how to make Invoke-Gherkin from within a Describe, so...
-&(Get-Module Pester){ $script:OuterPester = $Pester }
+# Calling this in a job so we don't monkey with the active pester state that's already running
 
-# We're going to call it a few times right up front instead:
-$results = invoke-gherkin (Join-Path $scriptRoot Examples\Validator\Validator.feature) -PassThru -Quiet
-$Mockery = invoke-gherkin (Join-Path $scriptRoot Examples\Validator\Validator.feature) -PassThru -Tag Mockery -Quiet
-$Examples = invoke-gherkin (Join-Path $scriptRoot Examples\Validator\Validator.feature) -PassThru -Tag Examples -Quiet
-$Example1 = invoke-gherkin (Join-Path $scriptRoot Examples\Validator\Validator.feature) -PassThru -Tag Example1 -Quiet
-$Example2 = invoke-gherkin (Join-Path $scriptRoot Examples\Validator\Validator.feature) -PassThru -Tag Example2 -Quiet
-$NamedScenario = invoke-gherkin (Join-Path $scriptRoot Examples\Validator\Validator.feature) -PassThru -ScenarioName "When something uses MyValidator" -Quiet
-$NotMockery = invoke-gherkin (Join-Path $scriptRoot Examples\Validator\Validator.feature) -PassThru -ExcludeTag Mockery -Quiet
+$job = Start-Job -ArgumentList $scriptRoot -ScriptBlock {
+    param ($scriptRoot)
 
-# put back the original state
-&(Get-Module Pester){ $script:Pester = $script:OuterPester }
+    New-Object psobject -Property @{
+        Results       = invoke-gherkin (Join-Path $scriptRoot Examples\Validator\Validator.feature) -PassThru -Quiet
+        Mockery       = invoke-gherkin (Join-Path $scriptRoot Examples\Validator\Validator.feature) -PassThru -Tag Mockery -Quiet
+        Examples      = invoke-gherkin (Join-Path $scriptRoot Examples\Validator\Validator.feature) -PassThru -Tag Examples -Quiet
+        Example1      = invoke-gherkin (Join-Path $scriptRoot Examples\Validator\Validator.feature) -PassThru -Tag Example1 -Quiet
+        Example2      = invoke-gherkin (Join-Path $scriptRoot Examples\Validator\Validator.feature) -PassThru -Tag Example2 -Quiet
+        NamedScenario = invoke-gherkin (Join-Path $scriptRoot Examples\Validator\Validator.feature) -PassThru -ScenarioName "When something uses MyValidator" -Quiet
+        NotMockery    = invoke-gherkin (Join-Path $scriptRoot Examples\Validator\Validator.feature) -PassThru -ExcludeTag Mockery -Quiet
+    }
+}
+
+$gherkin = $job | Wait-Job | Receive-Job
+Remove-Job $job
 
 Describe 'Invoke-Gerkin' {
     It 'Works on the Validator example' {
-        $results.PassedCount | Should Be $results.TotalCount
+        $gherkin.Results.PassedCount | Should Be $gherkin.Results.TotalCount
     }
 
     It 'Supports testing only scenarios with certain tags' {
-        $Mockery.PassedCount | Should Be $Mockery.TotalCount
-        $Mockery.TotalCount | Should BeLessThan $results.TotalCount
+        $gherkin.Mockery.PassedCount | Should Be $gherkin.Mockery.TotalCount
+        $gherkin.Mockery.TotalCount | Should BeLessThan $gherkin.Results.TotalCount
     }
 
     It 'Supports tagging examples' {
-        $Example1.PassedCount | Should Be $Example1.TotalCount
-        $Example1.TotalCount | Should BeLessThan $Examples.TotalCount
+        $gherkin.Example1.PassedCount | Should Be $gherkin.Example1.TotalCount
+        $gherkin.Example1.TotalCount | Should BeLessThan $gherkin.Examples.TotalCount
 
-        $Example2.PassedCount | Should Be $Example2.TotalCount
-        $Example2.TotalCount | Should BeLessThan $Examples.TotalCount
+        $gherkin.Example2.PassedCount | Should Be $gherkin.Example2.TotalCount
+        $gherkin.Example2.TotalCount | Should BeLessThan $gherkin.Examples.TotalCount
 
-        ($Example1.TotalCount + $Example2.TotalCount) | Should Be $Examples.TotalCount
+        ($gherkin.Example1.TotalCount + $gherkin.Example2.TotalCount) | Should Be $gherkin.Examples.TotalCount
     }
 
     It 'Supports excluding scenarios by tag' {
-        $NotMockery.PassedCount | Should Be $NotMockery.TotalCount
-        $NotMockery.TotalCount | Should BeLessThan $results.TotalCount
-        ($NotMockery.TotalCount + $Mockery.TotalCount) | Should Be $results.TotalCount
+        $gherkin.NotMockery.PassedCount | Should Be $gherkin.NotMockery.TotalCount
+        $gherkin.NotMockery.TotalCount | Should BeLessThan $gherkin.Results.TotalCount
+        ($gherkin.NotMockery.TotalCount + $gherkin.Mockery.TotalCount) | Should Be $gherkin.Results.TotalCount
     }
 
     It 'Supports running specific scenarios by name' {
-        $NamedScenario.PassedCount | Should Be $Mockery.TotalCount
+        $gherkin.NamedScenario.PassedCount | Should Be $gherkin.Mockery.TotalCount
     }
 }
