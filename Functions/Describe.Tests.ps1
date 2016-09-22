8d44c9cae7e0b722c2c1051a0840b9c32790dbd3
James W. Truher <jim.truher@microsoft.com>
2016-09-22 10:20:50 -0700

    Add test for error in AfterAll

--- Functions/Describe.Tests.ps1
+++ Functions/Describe.Tests.ps1
@@ -16,4 +16,37 @@ Describe 'Testing Describe' {
 
         { Describe Bogus } | Should Throw 'No test script block is provided'
     }
+    Context 'Testing Describe with an error in AfterAll' {
+        BeforeAll {
+            $Content1 = @"
+Describe "describe 1" {
+    AfterAll { throw "badness" }
+    It "test 1" { 1 | should be 1 }
+    It "test 2" { 1 | should be 1 }
+}
+"@
+            $Content2 = @"
+Describe "describe 2" {
+    It "test 3" { 1 | should be 1 }
+    It "test 4" { 1 | should be 1 }
+}
+"@
+            Setup -d ptest
+            setup -f ptest/t1.tests.ps1 -content $Content1
+            setup -f ptest/t2.tests.ps1 -content $Content2
+            $pesterBase = (get-module pester).modulebase
+            # isolate this in a new runspace because we're running
+            # invoke-pester, which will create a new TESTDRIVE
+            $ps = [powershell]::Create([System.Management.Automation.RunspaceMode]::NewRunspace)
+        }
+        AfterAll {
+            $ps.dispose()
+        }
+        It "Pester should return a failure in the case of a error in AfterAll" {
+            $r = $ps.AddScript("import-module $pesterBase; invoke-pester '$TESTDRIVE/ptest' -quiet -pass").Invoke()
+            $r.TotalCount  | should be 5
+            $r.PassedCount | should be 4
+            $r.FailedCount | should be 1
+        }
+    }
 }
