56e37538e3a254171cc2c252181aa78e9124c421
Adam Bertram <adbertram@gmail.com>
2016-11-09 14:43:28 -0600

    Added new-mockobject function

---
+++ Functions/New-MockObject.Tests.ps1
@@ -0,0 +1,10 @@
+describe 'New-MockObject' {
+
+    . "$PSScriptRoot\New-MockObject.ps1"
+    
+    it 'instantiates an object from a class with no public constructors' {
+        $type = 'Microsoft.PowerShell.Commands.Language'
+        New-MockObject -Type $type | should beoftype $type
+    }
+
+}
