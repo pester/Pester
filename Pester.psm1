f44b9d9e1d256a1335379baa44c5d19ab2780a3d
Adam Bertram <adbertram@gmail.com>
2016-11-11 14:16:29 -0600

    Added New-Mockobject to module

--- Pester.psm1
+++ Pester.psm1
@@ -646,6 +646,31 @@ function Set-ScriptBlockScope
     [scriptblock].GetProperty('SessionStateInternal', $flags).SetValue($ScriptBlock, $SessionStateInternal, $null)
 }
 
+function New-MockObject {
+    <#
+    .SYNOPSIS
+        This function instantiates a .NET object from a type. The assembly for the particular type must be
+        loaded.
+
+    .PARAMETER Type
+        The .NET type to create an object from.
+
+    .EXAMPLE
+        PS> $obj = New-MockObject -Type 'System.Diagnostics.Process'
+        PS> $obj.GetType().FullName
+            System.Diagnostics.Process
+    #>
+
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [type]$Type
+    )
+
+    [System.Runtime.Serialization.Formatterservices]::GetUninitializedObject($Type)
+
+}
+
 function Get-ScriptBlockScope
 {
     [CmdletBinding()]
