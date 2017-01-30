2e25735a6e44541ab432f5792a783781885192be
Joel Bennett <Jaykul@HuddledMasses.org>
2017-01-30 02:27:59 -0500

    Add a test for the scenario count which is currently failing

--- Functions/Gherkin.Tests.ps1
+++ Functions/Gherkin.Tests.ps1
@@ -45,12 +45,17 @@ Describe 'Invoke-Gherkin' {
     }
 
     It 'Supports excluding scenarios by tag' {
-        $gherkin.NotMockery.PassedCount | Should Be $gherkin.NotMockery.TotalCount
+        $gherkin.NotMockery.PassedCount | Should Be 10
         $gherkin.NotMockery.TotalCount | Should BeLessThan $gherkin.Results.TotalCount
         ($gherkin.NotMockery.TotalCount + $gherkin.Mockery.TotalCount) | Should Be $gherkin.Results.TotalCount
     }
 
     It 'Supports running specific scenarios by name' {
-        $gherkin.NamedScenario.PassedCount | Should Be $gherkin.Mockery.TotalCount
+        $gherkin.NamedScenario.PassedCount | Should Be 3
+    }
+
+    It 'Outputs the correct number of passed scenarios' {
+        $gherkin.Results.PassedScenarios | Should Be 2
+        $gherkin.NamedScenario.PassedScenarios | Should Be 1
     }
 }
