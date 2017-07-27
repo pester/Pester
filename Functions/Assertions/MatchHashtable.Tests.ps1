26c1c5d0d5e941210b42b115dd8e3e359d075a82
Stuart Leeks <stuart@leeks.net>
2017-07-27 21:08:04 +0100

    Add MatchHashtable assertion

---
+++ Functions/Assertions/MatchHashtable.Tests.ps1
@@ -0,0 +1,26 @@
+﻿Set-StrictMode -Version Latest
+
+InModuleScope Pester {
+
+    Describe "MatchHashtable" {
+        It "returns true for matching single item hashtable with same values" {
+            @{"a" = 1} | Should MatchHashtable @{"a" = 1}
+        }
+        It "returns true for hashtable with the same contents" {
+            @{"a" = 1; "b" = "test"} | Should MatchHashtable  @{"a" = 1; "b" = "test"}
+        }
+        It "returns true for hashtable with the same contents in different orders" {
+            @{"a" = 1; "b" = "test"} | Should MatchHashtable  @{"b" = "test"; "a" = 1}
+        }
+
+        It "returns false if hashtable differ in content" {
+            @{"a" = 1; "b" = "test"}  | Should Not MatchHashtable @{"a" = 1; "b" = "different value"}
+        }
+        It "returns false if hashtable differ in length - input2 longer" {
+            @{"a" = 1} | Should Not MatchHashtable  @{"a" = 1; "b" = "test"}
+        }
+        It "returns false if hashtable differ in length - input1 longer" {
+            @{"a" = 1; "b" = "test"}  | Should Not MatchHashtable  @{"a" = 1}
+        }
+    }
+}
---
+++ Functions/Assertions/MatchHashtable.ps1
@@ -0,0 +1,57 @@
+function PesterMatchHashtable($ActualValue, $ExpectedValue, [switch] $Negate) {
+   $message = FindMismatchedHashtableValue $ActualValue $ExpectedValue
+    $success = $message -eq $null;
+
+    if ($success) {
+        if ($Negate) {
+            #expecting failure
+            $success = $false
+            $message = "Expected: ${ActualValue} to not match the expression ${ExpectedValue}"
+        }
+        # else - we can just return success
+    }
+    else {
+        if ($Negate) {
+            # expecting failure
+            $success = $true
+            $message = ""
+        }
+        # else - we can just return failure
+    }
+    return New-Object psobject -Property @{
+        Succeeded      = $success
+        FailureMessage = $message
+    }
+}
+
+function FindMismatchedHashtableValue($ActualValue, $ExpectedValue) {
+    foreach($expectedKey in $ExpectedValue.Keys) {
+        if (-not($ActualValue.Keys -contains $expectedKey)){
+            return "Expected key: {$expectedKey}, but missing in actual"
+        }
+        $expectedItem = $ExpectedValue[$expectedKey]
+        $actualItem = $ActualValue[$expectedKey]
+        if (-not ($actualItem -eq $expectedItem)) {
+            return "Value differs for key {$expectedKey}. Expected value: {$expectedItem}, actual value: {$actualItem}"
+        }
+    }
+
+    foreach($actualKey in $ActualValue.Keys) {
+        if (-not($ExpectedValue.Keys -contains $actualKey)){
+            return "Actual key: {$actualKey}, but missing in expected"
+        }
+        $expectedItem = $ExpectedValue[$actualKey]
+        $actualItem = $ActualValue[$actualKey]
+        if (-not ($actualItem -eq $expectedItem)) {
+            return "Value differs for key {$actualKey}. Expected value: {$expectedItem}, actual value: {$actualItem}"
+        }
+    }
+}
+
+function NotPesterMatchHashtableFailureMessage($ActualValue, $ExpectedValue) {
+    return "Expected: ${ActualValue} to not match the expression ${ExpectedValue}"
+}
+
+Add-AssertionOperator -Name  MatchHashtable `
+    -Test  $function:PesterMatchHashtable
+    
