2e983c2d5e7da5f107ebf528878a1d037ba5d0c1
Stuart Leeks <stuart@leeks.net>
2017-07-27 20:46:35 +0100

    Add MatchArray assertions

---
+++ Functions/Assertions/MatchArray.Tests.ps1
@@ -0,0 +1,54 @@
+﻿Set-StrictMode -Version Latest
+
+InModuleScope Pester {
+    Describe "PesterMatchArrayUnordered" {
+        It "returns true for matching single item arrays" {
+            , @("a") | Should MatchArrayUnordered @("a")
+        }
+        It "returns true for matching single item and single item array" {
+            "a" | Should MatchArrayUnordered @("a")
+        }
+        It "returns true for matching single item array and single item" {
+            , (@("a")) | Should MatchArrayUnordered "a"
+        }
+        It "returns true for arrays with the same contents" {
+            , @("a", 1) | Should MatchArrayUnordered @("a", 1)
+        }
+        It "returns true for arrays with the same contents in different orders" {
+            , (@("a", 1)) | Should MatchArrayUnordered @(1, "a")
+        }
+
+        It "returns false if arrays differ in content" {
+            , (@(1)) | Should Not MatchArrayUnordered @(2)
+        }
+        It "returns false if arrays differ in length" {
+            , (@(1)) | Should Not MatchArrayUnordered @(1, 1)
+        }
+    }
+
+
+    Describe "PesterMatchArrayOrdered" {
+        It "returns true for matching single item arrays" {
+            , @("a") | Should MatchArrayOrdered @("a")
+        }
+        It "returns true for matching single item and single item array" {
+            "a" | Should MatchArrayOrdered @("a")
+        }
+        It "returns true for matching single item array and single item" {
+            , @("a") | Should MatchArrayOrdered  "a"
+        }
+        It "returns true for arrays with the same contents in the same order" {
+            , @("a", 1)  | Should MatchArrayOrdered  @("a", 1)
+        }
+        It "returns false for arrays with the same contents in different orders" {
+            , @("a", 1) | Should Not MatchArrayOrdered  @(1, "a")
+        }
+
+        It "returns false if arrays differ in content" {
+            , @(1)  | Should Not MatchArrayOrdered  @(2)
+        }
+        It "returns false if arrays differ in length" {
+            , @(1)  | Should Not MatchArrayOrdered  @(1, 1)
+        }
+    }
+}
---
+++ Functions/Assertions/MatchArray.ps1
@@ -0,0 +1,89 @@
+function PesterMatchArrayUnordered($ActualValue, $ExpectedValue, [switch] $Negate) {
+    $message = FindMismatchedValueUnordered $ActualValue $ExpectedValue
+    $success = $message -eq $null;
+
+    if ($success) {
+        if ($Negate) {
+            #expecting failure
+            $success = $false
+            $message = "Expected: ${ActualValue} to not match the expression ${ExpectedValue}"
+        }
+        # else - we can just return success
+    }
+    else {
+        if ($Negate) {
+            # expecting failure
+            $success = $true
+            $message = ""
+        }
+        # else - we can just return failure
+    }
+    return New-Object psobject -Property @{
+        Succeeded      = $success
+        FailureMessage = $message
+    }
+} 
+function FindMismatchedValueUnordered($ActualValue, $ExpectedValue) {
+    $ActualValue = @($ActualValue)
+    for ($i = 0; $i -lt $ExpectedValue.Length; $i++) {
+        if (-not($ActualValue -contains $ExpectedValue[$i])) {
+            return "Expected: {$ExpectedValue}. Actual: {$ActualValue}. Actual is missing item: $($ExpectedValue[$i])"
+        }
+    }
+    for ($i = 0; $i -lt $ActualValue.Length; $i++) {
+        if (-not($ExpectedValue -contains $ActualValue[$i])) {
+            return "Expected: {$ExpectedValue}. Actual: {$ActualValue}. Actual contains extra item: $($ActualValue[$i])"
+        }
+    }
+    if ($ActualValue.Length -ne $ExpectedValue.Length) {
+        return "Lengths differ. Expected length $($ExpectedValue.Length). Actual length $($ActualValue.Length) ";
+    }
+    return $null;
+}
+
+Add-AssertionOperator -Name  MatchArrayUnordered `
+    -Test  $function:PesterMatchArrayUnordered
+                      
+###################################################################################################
+function PesterMatchArrayOrdered($ActualValue, $ExpectedValue, [switch] $Negate) {
+
+    $message = FindMismatchedValueOrdered $ActualValue $ExpectedValue
+    $success = $message -eq $null;
+
+    if ($success) {
+        if ($Negate) {
+            #expecting failure
+            $success = $false
+            $message = "Expected: ${ActualValue} to not match the expression ${ExpectedValue}"
+        }
+        # else - we can just return success
+    }
+    else {
+        if ($Negate) {
+            # expecting failure
+            $success = $true
+            $message = ""
+        }
+        # else - we can just return failure
+    }
+    return New-Object psobject -Property @{
+        Succeeded      = $success
+        FailureMessage = $message
+    }
+} 
+
+function FindMismatchedValueOrdered($ActualValue, $ExpectedValue) {
+    $ActualValue = @($ActualValue)
+    for ($i = 0; $i -lt $ExpectedValue.Length -and $i -lt $ActualValue.Length; $i++) {
+        if (-not($ActualValue[$i] -eq $ExpectedValue[$i])) {
+            return "Differs at index $i. Expected: {$ExpectedValue}. Actual: {$ActualValue}."
+        }
+    }
+    if ($ActualValue.Length -ne $ExpectedValue.Length) {
+        return "Lengths differ - Expected length $($ExpectedValue.Length), actual length $($ActualValue.Length)";
+    }
+    return $null;
+}
+
+Add-AssertionOperator -Name  MatchArrayOrdered `
+    -Test  $function:PesterMatchArrayOrdered
