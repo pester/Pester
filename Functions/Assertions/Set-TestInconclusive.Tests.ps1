61f6dd3e795bffd73698f25e8ee3b00603b5514d
Jakub Jareš <me@jakubjares.com>
2018-04-28 11:09:02 +0200

    Update Set-TestInconclusive.Tests.ps1

--- Functions/Assertions/Set-TestInconclusive.Tests.ps1
+++ Functions/Assertions/Set-TestInconclusive.Tests.ps1
@@ -3,11 +3,11 @@ Set-StrictMode -Version Latest
 Describe "Ensuring Set-TestInconclusive is deprecated" {
     Context "Set-TestInconclusive calls Set-ItResult -Inconclusive" {
         InModuleScope -Module Pester {
-            Mock Set-ItResult { } -ParameterFilter { $Inconclusive -eq $true }
             It "Set-TestInconclusive calls Set-ItResult internally" {
+                Mock Set-ItResult { }
                 try { Set-TestInconclusive }
                 catch {}
-                Assert-MockCalled Set-ItResult
+                Assert-MockCalled Set-ItResult -ParameterFilter { $Inconclusive -eq $true }
             }
         }
     }
