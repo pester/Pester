ec8a540b74e6268198e07921509240cefd184583
Brian Bunke <brianbunke+github@gmail.com>
2018-10-13 22:46:14 -0700

    Update function help

--- Functions/Get-ShouldOperator.ps1
+++ Functions/Get-ShouldOperator.ps1
@@ -4,10 +4,10 @@ function Get-ShouldOperator {
     Display the assertion operators available for use with Should.
 
     .DESCRIPTION
-    Get-ShouldOperator returns a list of available Should parameters
-    -- and their aliases and help docs -- to help you craft the tests you need.
+    Get-ShouldOperator returns a list of available Should parameters,
+    their aliases, and examples to help you craft the tests you need.
 
-    Get-ShouldOperator will list all available assertions,
+    Get-ShouldOperator will list all available operators,
     including any registered by the user with Add-AssertionOperator.
 
     .NOTES
@@ -18,9 +18,12 @@ function Get-ShouldOperator {
 
     .EXAMPLE
     Get-ShouldOperator
+    Return all available Should assertion operators and their aliases.
 
     .EXAMPLE
     Get-ShouldOperator -Name Be
+    Return help examples for the Be assertion operator.
+    -Name is a dynamic parameter that tab completes all available options.
 
     .LINK
     https://github.com/Pester/Pester
@@ -69,7 +72,7 @@ function Get-ShouldOperator {
 
     END {
         If ($Name) {
-            $help = Get-Help (Get-AssertionOperatorEntry $Name).InternalName -Full -ErrorAction SilentlyContinue
+            $help = Get-Help (Get-AssertionOperatorEntry $Name).InternalName -Examples -ErrorAction SilentlyContinue
 
             If (($help | Measure-Object).Count -ne 1) {
                 # No way to stop Get-Help if there isn't an exact match
