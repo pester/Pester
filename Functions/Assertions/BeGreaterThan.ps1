254699c80e231777c1cc7e03758064219877ef46
Brian Bunke <brianbunke+github@gmail.com>
2018-10-17 21:06:28 -0700

    Rename and add help for BeGreaterThan

--- Functions/Assertions/BeGreaterThan.ps1
+++ Functions/Assertions/BeGreaterThan.ps1
@@ -1,7 +1,15 @@
-function PesterBeGreaterThan($ActualValue, $ExpectedValue, [switch] $Negate, [string] $Because)
-{
+function Should-BeGreaterThan($ActualValue, $ExpectedValue, [switch] $Negate, [string] $Because) {
+<#
+.SYNOPSIS
+Asserts that a number (or other comparable value) is greater than an expected value.
+Uses PowerShell's -gt operator to compare the two values.
+
+.EXAMPLE
+2 | Should -BeGreaterThan 0
+This test passes, as PowerShell evaluates `2 -gt 0` as true.
+#>
     if ($Negate) {
-        return PesterBeLessOrEqual -ActualValue $ActualValue -ExpectedValue $ExpectedValue -Negate:$false -Because $Because
+        return Should-BeLessOrEqual -ActualValue $ActualValue -ExpectedValue $ExpectedValue -Negate:$false -Because $Because
     }
 
     if ($ActualValue -le $ExpectedValue) {
@@ -17,10 +25,22 @@ function PesterBeGreaterThan($ActualValue, $ExpectedValue, [switch] $Negate, [st
 }
 
 
-function PesterBeLessOrEqual($ActualValue, $ExpectedValue, [switch] $Negate, [string] $Because)
-{
-    if ($Negate) {
-        return PesterBeGreaterThan -ActualValue $ActualValue -ExpectedValue $ExpectedValue -Negate:$false -Because $Because
+function Should-BeLessOrEqual($ActualValue, $ExpectedValue, [switch] $Negate, [string] $Because) {
+<#
+.SYNOPSIS
+Asserts that a number (or other comparable value) is lower than, or equal to an expected value.
+Uses PowerShell's -le operator to compare the two values.
+
+.EXAMPLE
+1 | Should -BeLessOrEqual 10
+This test passes, as PowerShell evaluates `1 -le 10` as true.
+
+.EXAMPLE
+10 | Should -BeLessOrEqual 10
+This test also passes, as PowerShell evaluates `10 -le 10` as true.
+#>
+if ($Negate) {
+        return Should-BeGreaterThan -ActualValue $ActualValue -ExpectedValue $ExpectedValue -Negate:$false -Because $Because
     }
 
     if ($ActualValue -gt $ExpectedValue) {
@@ -35,20 +55,19 @@ function PesterBeLessOrEqual($ActualValue, $ExpectedValue, [switch] $Negate, [st
     }
 }
 
-Add-AssertionOperator -Name  BeGreaterThan `
-                      -Test  $function:PesterBeGreaterThan `
-                      -Alias 'GT'
+Add-AssertionOperator -Name         BeGreaterThan `
+                      -InternalName Should-BeGreaterThan `
+                      -Test         ${function:Should-BeGreaterThan} `
+                      -Alias        'GT'
 
-Add-AssertionOperator -Name  BeLessOrEqual `
-                      -Test  $function:PesterBeLessOrEqual `
-                      -Alias 'LE'
+Add-AssertionOperator -Name         BeLessOrEqual `
+                      -InternalName Should-BeLessOrEqual `
+                      -Test         ${function:Should-BeLessOrEqual} `
+                      -Alias        'LE'
 
 #keeping tests happy
-function PesterBeGreaterThanFailureMessage() {  }
-function NotPesterBeGreaterThanFailureMessage() { }
+function ShouldBeGreaterThanFailureMessage() {  }
+function NotShouldBeGreaterThanFailureMessage() { }
 
-function PesterBeLessOrEqualFailureMessage() {  }
-function NotPesterBeLessOrEqualFailureMessage() { }
-
-
-
+function ShouldBeLessOrEqualFailureMessage() {  }
+function NotShouldBeLessOrEqualFailureMessage() { }
