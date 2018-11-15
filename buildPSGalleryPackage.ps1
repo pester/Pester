78f54d59bcb9b8604cd8553442fab59bd664ac79
nohwnd <me@jakubjares.com>
2018-11-15 11:50:14 +0100

    plumbing for signing the module

--- buildPSGalleryPackage.ps1
+++ buildPSGalleryPackage.ps1
@@ -5,23 +5,18 @@ $baseDir = $PSScriptRoot
 try
 {
     $buildDir = "$baseDir\build\psgallery\Pester"
-    if (Test-Path $buildDir) {
-        Write-Verbose 'Removing build folder'
-        Remove-Item -LiteralPath $buildDir -Recurse -Force -Confirm:$false -Verbose
-    }
-
     $null = New-Item -Path $buildDir -ItemType Directory -Verbose
 
     Write-Verbose "Copying release files to build folder '$buildDir'"
     Copy-Item $baseDir\Pester.ps?1          $buildDir\
     Copy-Item $baseDir\LICENSE              $buildDir\
     Copy-Item $baseDir\nunit_schema_2.5.xsd $buildDir\
-    Copy-Item $baseDir\bin                  $buildDir\ -Recurse -Exclude *.tests.ps1
-    Copy-Item $baseDir\Functions            $buildDir\ -Recurse -Exclude *.tests.ps1
-    Copy-Item $baseDir\Dependencies         $buildDir\ -Recurse -Exclude *.tests.ps1
-    Copy-Item $baseDir\Snippets             $buildDir\ -Recurse -Exclude *.tests.ps1
-    Copy-Item $baseDir\en-US                $buildDir\ -Recurse -Exclude *.tests.ps1
-    Copy-Item $baseDir\lib                  $buildDir\ -Recurse -Exclude *.tests.ps1
+    Copy-Item $baseDir\bin                  $buildDir\ -Recurse 
+    Copy-Item $baseDir\Functions            $buildDir\ -Recurse
+    Copy-Item $baseDir\Dependencies         $buildDir\ -Recurse
+    Copy-Item $baseDir\Snippets             $buildDir\ -Recurse
+    Copy-Item $baseDir\en-US                $buildDir\ -Recurse
+    Copy-Item $baseDir\lib                  $buildDir\ -Recurse
 
     Write-Verbose 'Copy complete. Contents:'
     Get-ChildItem $buildDir -Recurse | Out-Host
---
+++ cleanUpBeforeBuild.ps1
@@ -0,0 +1,22 @@
+# cleaning up remains of previous builds 
+# examples and all test files 
+# in the next step we sigin all scripts so
+# we want to reduce how many files will be signed
+#
+# this clean up is not removing all unneeded files,
+# it only removes the main parts
+# each package then decides what will be part of it
+
+$buildDir = "$PSScriptRoot\build"
+if (Test-Path $buildDir) {
+    Write-Verbose "Removing build dir"
+    Remove-Item $buildDir -Recurse -Force -Confirm:$false -Verbose
+}
+
+Write-Verbose "Removing all examples"
+Remove-Item "$PSScriptRoot\Examples" -Recurse -Force -Confirm:$false -Verbose
+Write-Verbose "Removing docs"
+Remove-Item "$PSScriptRoot\doc" -Recurse -Force -Confirm:$false -Verbose
+
+Write-Verbose "Removing all Test Files"
+Get-ChildItem $PSScriptRoot -Recurse -Filter *.Tests.ps1 | Remove-Item -Force -Verbose
