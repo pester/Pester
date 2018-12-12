72c60212f1b9f0d5af92fcac87492e1ee3ff344b
nohwnd <me@jakubjares.com>
2018-12-12 21:19:15 +0100

    Fix symlink deleting test and comment it out

--- Functions/TestDrive.Tests.ps1
+++ Functions/TestDrive.Tests.ps1
@@ -169,22 +169,36 @@ InModuleScope Pester {
     }
 }
 
-InModuleScope Pester {
-    Describe "Clear-TestDrive" {
-        It "deletes symbolic links in TestDrive" {
 
-            $root    = "TestDrive:\";
-            $source  = "$root\source";
-            $symlink = "$root\symlink";
+# # this works correctly but needs administrator privileges to create the symlinks
+# # so I comment it out till I decide what to do with it, running always as admin just
+# # to test this is imho not a good idea. Tested on powershell 2 and 5 -- nohwnd
+# InModuleScope Pester {
+#     Describe "Clear-TestDrive" {
 
-            $null = New-Item -Type Directory -Path $source;
-            $null = New-Item -Type SymbolicLink -Path $symlink -Value $source;
+#         It "deletes symbolic links in TestDrive" {
+            
+#             # using non-powershell paths here because we need to interop with cmd below
+#             $root    = (Get-PsDrive 'TestDrive').Root
+#             $source  = "$root\source"
+#             $symlink = "$root\symlink"
 
-            { Clear-TestDrive } | Should -Not -Throw;
+#             $null = New-Item -Type Directory -Path $source
 
-            $count = @(Get-ChildItem -Path $root).Length;
-            $count | Should -Be 0;
+#             if ($PSVersionTable.PSVersion.Major -ge 5) {
+#                 # native support for symlinks was added in PowerShell 5
+#                 $null = New-Item -Type SymbolicLink -Path $symlink -Value $source
+#                 write-host (ls TestDrive:\ -force | out-string)
+#             }
+#             else {
+#                 $null = cmd /c mklink /D $symlink $source
+#             }
 
-        }
-    }
-}
+#             @(Get-ChildItem -Path $root).Length | Should -Be 2 -Because "a pre-requisite is that directory and symlink to it is in place"
+
+#             Clear-TestDrive 
+
+#             @(Get-ChildItem -Path $root).Length | Should -Be 0 -Because "everything should be deleted including symlinks"
+#         }
+#     }
+# }
