f2e4e3c904a69aa26bda9c3fefb6fa6d5a54af3b
nohwnd <me@jakubjares.com>
2018-12-20 13:12:40 +0100

    Fix expanding properties on arrays of objects in PowerShell 2

--- Functions/Gherkin.Tests.ps1
+++ Functions/Gherkin.Tests.ps1
@@ -290,7 +290,9 @@ Describe "Check test results of steps" -Tag Gherkin {
     $gherkin = $job | Wait-Job | Receive-Job
     Remove-Job $job
 
-    $testResults = $gherkin.Results.TestResult.Result
+    $testResults = $gherkin.Results | 
+        Select-Object -ExpandProperty TestResult | 
+        Select-Object -ExpandProperty Result
 
     It "Should have the expected number of test results" {
         $testResults.Count | Should -Be 15
