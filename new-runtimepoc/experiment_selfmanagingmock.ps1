6e920511b983d35584f10233d7f90ab51b6a92ff
nohwnd <me@jakubjares.com>
2019-01-12 16:49:45 +0100

    Self-scoping mock

--- new-runtimepoc/experiment_selfmanagingmock.ps1
+++ new-runtimepoc/experiment_selfmanagingmock.ps1
@@ -1,23 +1,17 @@
-function Import-Sb {
-    [CmdletBinding()]
-    param(
-        [Parameter(Mandatory = $true)]
-        $sb,
-        # [Parameter(Mandatory=$true)]
-        [Management.Automation.SessionState] $SessionState
-    )
+# so this is a mock that self-manages
+# based on the scopes, this allows us
+# to let powershell manage the life-time of
+# the bootstrap function and alias automatically
+# so a Mock defined in It will die with the
+# end of It (or more precisely the scope) it
+# is defined in, just like functions do
+# this prevents us from supporting the current
+# behavior of leaking mocks outside of It,
+# but that's a good thing and it lifts the burden
+# of managing the scope correctly
 
-    $flags = [System.Reflection.BindingFlags]'Instance,NonPublic'
-    $SessionStateInternal = $SessionState.GetType().GetProperty('Internal', $flags).GetValue($SessionState, $null)
-
-    # attach the original session state to the wrapper scriptblock
-    # making it invoke in the caller session state
-    $sb.GetType().GetProperty('SessionStateInternal', $flags).SetValue($sb, $SessionStateInternal, $null)
-
-    # dot source the caller bound scriptblock which imports it into user scope
-    . $sb
-}
-
+# I also keep the aliases because they provide
+# are resolved first and allow inter-file mocking
 
 get-module m | remove-module
 New-Module -name m -ScriptBlock {
@@ -25,16 +19,31 @@ New-Module -name m -ScriptBlock {
         [CmdletBinding()]
         param($name, $sb)
 
-        $mockBody = [scriptblock]::Create("
-        function Mock_$name { Write-Host 'this is mock' }
-        Set-Alias -Name $name -Value Mock_$Name
+        $sb = [scriptblock]::Create("
+            function mock_$name { $sb }
+            Set-Alias -Name $name -Value mock_$name
         ")
 
+        # $sb = {
+        #     # $ExecutionContext.SessionState
+        #     function mock_a { write-host 'this is mock' }
+        #     Set-Alias -Name a -Value mock_a
+        # }
 
-        Import-Sb -SessionState $PSCmdlet.SessionState -sb $mockBody
+
+        $flags = [System.Reflection.BindingFlags]'Instance,NonPublic'
+        $SessionStateInternal = $pscmdlet.SessionState.GetType().GetProperty('Internal', $flags).GetValue($pscmdlet.SessionState, $null)
+
+        # attach the original session state to the wrapper scriptblock
+        # making it invoke in the caller session state
+        $sb.GetType().GetProperty('SessionStateInternal', $flags).SetValue($sb, $SessionStateInternal, $null)
+
+        # dot source the caller bound scriptblock which imports it into user scope
+        . $sb
     }
 } | Import-Module
 
+get-date
 & {
     function a {
         "real"
@@ -42,7 +51,9 @@ New-Module -name m -ScriptBlock {
     a
     & {
         m a { "mock" }
+        write-host "calling a"
         a
     }
+
     a
 }
