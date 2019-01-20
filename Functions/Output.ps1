d75141e2f2368cda4e66e147b38239055d4e4691
nohwnd <me@jakubjares.com>
2019-01-20 12:33:02 +0100

    Fix filer message

--- Functions/Output.ps1
+++ Functions/Output.ps1
@@ -97,8 +97,8 @@ function Write-PesterStart {
         if ($PesterState.TestNameFilter) {
             $message += $ReportStrings.FilterMessage -f "$($PesterState.TestNameFilter)"
         }
-        if ($PesterState.PesterOption.ScriptBlockFilter) {
-            $m = $(foreach ($m in $PesterState.PesterOption.ScriptBlockFilter) { "$($m.Path):$($m.Line)" }) -join ", "
+        if ($PesterState.ScriptBlockFilter) {
+            $m = $(foreach ($m in $PesterState.ScriptBlockFilter) { "$($m.Path):$($m.Line)" }) -join ", "
             $message += $ReportStrings.FilterMessage -f $m
         }
         if ($PesterState.TagFilter) {
