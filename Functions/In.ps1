e39edb80fd25764fac17ee272e2ec11274323beb
Brett Miller <brett@millerb.co.uk>
2019-02-05 12:23:35 +0000

    Function: In - Added Online Link to CBH

--- Functions/In.ps1
+++ Functions/In.ps1
@@ -1,21 +1,24 @@
 function In {
     <#
-.SYNOPSIS
-A convenience function that executes a script from a specified path.
+    .SYNOPSIS
+    A convenience function that executes a script from a specified path.
 
-.DESCRIPTION
-Before the script block passed to the execute parameter is invoked,
-the current location is set to the path specified. Once the script
-block has been executed, the location will be reset to the location
-the script was in prior to calling In.
+    .DESCRIPTION
+    Before the script block passed to the execute parameter is invoked,
+    the current location is set to the path specified. Once the script
+    block has been executed, the location will be reset to the location
+    the script was in prior to calling In.
 
-.PARAMETER Path
-The path that the execute block will be executed in.
+    .PARAMETER Path
+    The path that the execute block will be executed in.
 
-.PARAMETER execute
-The script to be executed in the path provided.
+    .PARAMETER execute
+    The script to be executed in the path provided.
 
-#>
+    .LINK
+    https://github.com/pester/Pester/wiki/In
+
+    #>
     [CmdletBinding()]
     param(
         $path,
