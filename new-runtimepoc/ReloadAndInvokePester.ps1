d970d2bd37ab9b51479f41a577d241757bfcf545
nohwnd <me@jakubjares.com>
2019-05-01 13:55:09 +0200

    Move implementation to Should -Invoke and add Assert-Mock called shim function

--- new-runtimepoc/ReloadAndInvokePester.ps1
+++ new-runtimepoc/ReloadAndInvokePester.ps1
@@ -14,10 +14,12 @@ $global:PesterDebugPreference = @{
 }
 
 $excludePath = "*/demo/*"
+$excludePath =""
 $excludeTags = "Help", "VersionChecks", "Formatting"
 
 $path = "C:\Projects\Pester_main"
 #$path = "./Functions/Assertions/PesterThrow.Tests.ps1"
+$path = "C:\projects\pester_main\demo\mocking\CountingMocks.Tests.ps1"
 
 
 $script:r = $null
