28ffdcb6713f2a91bf9e6de50d923e89480df2b9
Christoph Bergmeister <c.bergmeister@gmail.com>
2019-09-17 18:44:33 +0100

    First simple prototype that merged Functions into '# Functions' string in psm1 TODO: handle insertion of $Script:PesterRoot = & $SafeCommands['Split-Path'] -Path $MyInvocation.MyCommand.Path at the right place and deletion of dot sourcing lines

---
+++ build.ps1
@@ -0,0 +1,16 @@
+$PowerShellScriptsToBeMerged = Get-ChildItem -Path (Join-Path $PSScriptRoot Functions) -Filter '*.ps1' -Exclude '*.Tests.ps1' -Recurse
+$mergedContent = ''
+foreach ($powerShellScriptToBeMerged in $PowerShellScriptsToBeMerged) {
+    $scriptContentToBeMerged = Get-Content -Path $powerShellScriptToBeMerged.FullName -Raw
+    $mergedContent += @"
+#region $($powerShellScriptToBeMerged.BaseName)
+$scriptContentToBeMerged
+#endregion
+
+"@
+}
+
+$mainModulePath = Join-Path $PSScriptRoot 'Pester.psm1'
+$contentOfMainModule = Get-Content -Path $mainModulePath
+$contentOfMainModule = $contentOfMainModule.Replace('# Functions', $mergedContent)
+Set-Content -Path $mainModulePath -Value $contentOfMainModule
