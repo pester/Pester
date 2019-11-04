924f4ea54b77353bf6bc945c7562d2392106b565
nohwnd <me@jakubjares.com>
2019-11-04 12:43:59 +0100

    Don't process code coverage on every file

--- Functions/Coverage.Plugin.ps1
+++ Functions/Coverage.Plugin.ps1
@@ -18,12 +18,13 @@ function Get-CoveragePlugin {
         if ($Context.Block.Root.PluginData.ContainsKey('Coverage')) {
             $breakpoints = $Context.Block.Root.PluginData.Coverage.CommandCoverage
 
-            $coverageReport = Get-CoverageReport -CommandCoverage $breakpoints
-            $Context.Block.Root.PluginData.Coverage.CoverageReport = $coverageReport
+            # todo: the following block should rather be taken at all the containers that are in the run. We should not need to calculate the coverage for each file separately because that will make it result in very low % coverage due to all files being included in the run.
+            # $coverageReport = Get-CoverageReport -CommandCoverage $breakpoints
+            # $Context.Block.Root.PluginData.Coverage.CoverageReport = $coverageReport
 
-            # TODO: Is the duration correct?
-            $jaCoCoReport = Get-JaCoCoReportXml -CommandCoverage $breakpoints -TotalMilliseconds $Context.Block.Duration.TotalMilliseconds -CoverageReport $coverageReport
-            $jaCoCoReport | & $SafeCommands['Out-File'] 'coverage.xml' -Encoding UTF8
+            # # TODO: Is the duration correct?
+            # $jaCoCoReport = Get-JaCoCoReportXml -CommandCoverage $breakpoints -TotalMilliseconds $Context.Block.Duration.TotalMilliseconds -CoverageReport $coverageReport
+            # $jaCoCoReport | & $SafeCommands['Out-File'] 'coverage.xml' -Encoding UTF8
 
             Exit-CoverageAnalysis -CommandCoverage $breakpoints
         }
