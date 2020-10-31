45c75ff48124e41460edeb7d1711d62eedd81ad7
asears <asears@users.noreply.github.com>
2020-10-31 05:51:44 -0400

    rollback getNugetExe.ps1
    Signed-off-by: asears <asears@users.noreply.github.com>

--- publish/getNugetExe.ps1
+++ publish/getNugetExe.ps1
@@ -7,23 +7,23 @@ try {
     # I download a version from the web and try that
 
     $nugetPath = "$psScriptRoot\vendor\tools\nuget.exe"
-    Write-Output "Nuget path ${nugetPath}"
+    Write-Host Nuget path $nugetPath
 
     $nugetFound = Test-Path $nugetPath
     if ($nugetFound) {
-        Write-Output "Found local nuget.exe running it"
+        Write-Host Found local nuget.exe running it
         $nugetRunnable = $false
         try {
-            &$nugetPath | Select-Object -First 1
+            &$nugetPath | select -First 1
             $nugetRunnable = $true
         }
         catch {
-            Write-Output 'Could not run local nuget.exe. Failed with ("$_")'
+            Write-Host Could not run local nuget.exe. Failed with ("$_")
             Remove-Item $nugetPath -Force -Confirm:$false -Verbose
         }
     }
     else {
-        Write-Output "Local nuget.exe not found"
+        Write-Host Local nuget.exe not found
     }
 
     if (-not $nugetFound -or -not $nugetRunnable) {
@@ -33,7 +33,7 @@ try {
         }
 
         Invoke-WebRequest -Uri "https://dist.nuget.org/win-x86-commandline/v4.4.1/nuget.exe" -OutFile $nugetPath -Verbose
-        "Downloaded $(&$nugetPath | Select-Object -First 1)"
+        "Downloaded $(&$nugetPath | select -First 1)"
     }
 
 }
