717ed9a4c907c2014f65dd5a7662cee0e29afccf
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2020-11-16 21:55:36 +0000

    Added comment to Invoke-InNewProcess

--- tst/Pester.RSpec.InNewProcess.ts.ps1
+++ tst/Pester.RSpec.InNewProcess.ts.ps1
@@ -19,7 +19,8 @@ function Invoke-InNewProcess ([ScriptBlock] $ScriptBlock) {
     # get the path of the currently loaded Pester to re-import it in the child process
     $pesterPath = Get-Module Pester | Select-Object -ExpandProperty Path
     $powershell = Get-Process -Id $pid | Select-Object -ExpandProperty Path
-    # run the test in a separate process to be able to grab all the output
+    # run any scriptblock in a separate process to be able to grab all the output
+    # doesn't enforce Invoke-Pester usage so we can test other public functions directly
     $command = {
         param ($PesterPath, [ScriptBlock] $ScriptBlock)
         Import-Module $PesterPath
