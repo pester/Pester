d663caaea74072179ff6cfc36adf613aef175449
asears <asears@users.noreply.github.com>
2021-03-06 20:21:06 -0500

    disable nullable check, implement more tests

--- src/csharp/PesterTests/FactoryTests.cs
+++ src/csharp/PesterTests/FactoryTests.cs
@@ -1,12 +1,9 @@
 using Pester;
+using System.Management.Automation;
 using Xunit;
 
 namespace PesterTests
 {
-    /// <summary>
-    /// Factory Tests
-    /// TODO test implementations
-    /// </summary>
     public class FactoryTests
     {
         [Fact]
@@ -16,62 +13,9 @@ namespace PesterTests
             var value = "test";
 
             var actual = Factory.CreateNoteProperty(name, value);
-        }
-
-        [Fact]
-        public void Test_CreateDictionary()
-        {
-            var actual = Factory.CreateDictionary();
-        }
-
-        [Fact]
-        public void Test_CreateRuntimeDefinedParameterDictionary()
-        {
-            var actual = Factory.CreateDictionary();
-        }
-
-        [Fact]
-        public void Test_CreateCollection()
-        {
-            var actual = Factory.CreateCollection();
-        }
-
-        [Fact]
-        public void Test_CreateShouldErrorRecord()
-        {
-            string message = null;
-            string file = null;
-            string line = null;
-            string lineText = null;
-            bool terminating = false;
-
-            var actual =
-                Factory
-                    .CreateShouldErrorRecord(message,
-                    file,
-                    line,
-                    lineText,
-                    terminating);
-        }
-
-        [Fact]
-        public void Test_CreateErrorRecord()
-        {
-            string errorId = null;
-            string message = null;
-            string file = null;
-            string line = null;
-            string lineText = null;
-            bool terminating = false;
-
-            var actual =
-                Factory
-                    .CreateErrorRecord(errorId,
-                    message,
-                    file,
-                    line,
-                    lineText,
-                    terminating);
+            Assert.IsType<PSNoteProperty>(actual);
+            Assert.Equal(name, actual.Name);
+            Assert.Equal(value, actual.Value);
         }
     }
 }
