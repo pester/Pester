fbf4aae6ef1c644d2f2ab755854b00ad31469030
nohwnd <me@jakubjares.com>
2021-04-18 23:27:22 +0200

    qoutes?

--- build.ps1
+++ build.ps1
@@ -162,7 +162,7 @@ Copy-Content -Content $content
 
 
 if ($Clean) {
-    $manifest = Test-ModuleManifest -Path $PSScriptRoot/bin/Pester.psd1
+    $manifest = Test-ModuleManifest -Path "$PSScriptRoot/bin/Pester.psd1"
     dotnet build "$PSScriptRoot/src/csharp/Pester.sln" --configuration Release -p:VersionPrefix="$($manifest.Version)" -p:VersionSuffix="$($manifest.PrivateData.PSData.Prerelease)"
     if (0 -ne $LASTEXITCODE) {
         throw "build failed!"
