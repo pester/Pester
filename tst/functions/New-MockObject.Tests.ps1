c86669422aee1266e123d7ec3121b3d04d60bd60
Ace Olszowka <aolszowka@gmail.com>
2021-04-29 07:06:10 -0600

    Move Into Context Block as Per Feedback from @fflaten

--- tst/functions/New-MockObject.Tests.ps1
+++ tst/functions/New-MockObject.Tests.ps1
@@ -6,25 +6,25 @@ describe 'New-MockObject' {
         $type = 'Microsoft.PowerShell.Commands.Language'
         New-MockObject -Type $type | should -beoftype $type
     }
-}
 
-Describe 'New-MockObject-With-Properties' {
-    # System.Diagnostics.Process.Id is normally read only, using the
-    # Properties switch in New-MockObject should allow us to mock these.
-    it 'Fails with just a normal mock' {
-        $mockedProcess = New-MockObject -Type 'System.Diagnostics.Process'
+    Context 'Modifying readonly-properties' {
+        # System.Diagnostics.Process.Id is normally read only, using the
+        # Properties switch in New-MockObject should allow us to mock these.
+        it 'Fails with just a normal mock' {
+            $mockedProcess = New-MockObject -Type 'System.Diagnostics.Process'
 
-        { $mockedProcess.Id = 123 } | Should -Throw
-    }
+            { $mockedProcess.Id = 123 } | Should -Throw
+        }
 
-    it 'Works when you mock the property' {
-        $mockedProcess = New-MockObject -Type 'System.Diagnostics.Process' -Properties @{Id = 123 }
+        it 'Works when you mock the property' {
+            $mockedProcess = New-MockObject -Type 'System.Diagnostics.Process' -Properties @{Id = 123 }
 
-        $mockedProcess.Id | Should -Be 123
-    }
+            $mockedProcess.Id | Should -Be 123
+        }
 
-    it 'Should preserve types' {
-        $mockedProcess = New-MockObject -Type 'System.Diagnostics.Process' -Properties @{Id = 123 }
-        $mockedProcess.Id | Should -BeOfType [int]
+        it 'Should preserve types' {
+            $mockedProcess = New-MockObject -Type 'System.Diagnostics.Process' -Properties @{Id = 123 }
+            $mockedProcess.Id | Should -BeOfType [int]
+        }
     }
 }
