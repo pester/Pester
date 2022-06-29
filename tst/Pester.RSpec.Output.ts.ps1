ee78518594834519738d5e24dc485d6319cbd50a
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2022-06-29 20:37:28 +0000

    make output test helper-function generic

--- tst/Pester.RSpec.Output.ts.ps1
+++ tst/Pester.RSpec.Output.ts.ps1
@@ -1,4 +1,4 @@
-﻿param ([switch] $PassThru)
+param ([switch] $PassThru)
 
 Get-Module Pester.Runtime, Pester.Utility, P, Pester, Axiom, Stack | Remove-Module
 
@@ -21,77 +21,75 @@ $global:PesterPreference = @{
 }
 $PSDefaultParameterValues = @{}
 
-function Invoke-PesterInProcess ([ScriptBlock] $ScriptBlock, [ScriptBlock] $Setup) {
+function Invoke-InNewProcess ([ScriptBlock] $ScriptBlock) {
     # get the path of the currently loaded Pester to re-import it in the child process
     $pesterPath = Get-Module Pester | Select-Object -ExpandProperty Path
     $powershell = Get-Process -Id $pid | Select-Object -ExpandProperty Path
-    # run the test in a separate process to be able to grab all the output
+    # run any scriptblock in a separate process to be able to grab all the output
+    # doesn't enforce Invoke-Pester usage so we can test other public functions directly
     $command = {
-        param ($PesterPath, [ScriptBlock] $ScriptBlock, [ScriptBlock] $Setup)
+        param ($PesterPath, [ScriptBlock] $ScriptBlock)
         Import-Module $PesterPath
 
-        # Modify environment
-        . $Setup
-
-        $container = New-PesterContainer -ScriptBlock $ScriptBlock
-        Invoke-Pester -Container $container
+        . $ScriptBlock
     }.ToString()
 
     # we need to escape " with \" because otherwise the " are eaten when the process we are starting recieves them
-    $cmd = "& { $command } -PesterPath ""$PesterPath"" -ScriptBlock { $($ScriptBlock -replace '"','\"') } -Setup { $($Setup -replace '"','\"') }"
+    $cmd = "& { $command } -PesterPath ""$PesterPath"" -ScriptBlock { $($ScriptBlock -replace '"','\"') }"
     & $powershell -NoProfile -ExecutionPolicy Bypass -Command $cmd
 }
 
 
 
 i -PassThru:$PassThru {
-    b "Output in VSCode mode" {
-
+    b 'Output in VSCode mode' {
         # VSCode-powershell Problem Matcher pattern
         # Storing the original pattern below for easier comparison and maintenance
         $titlePattern = ('^\\s*(?:\\[-\\]\\s+)(.*?)(?:\\s+\\d+\\.?\\d*\\s*m?s)(?:\\s+\\(\\d+\\.?\\d*m?s\\|\\d+\\.?\\d*m?s\\))?\\s*$' -replace '\\\\', '\')
         $atPattern = ('^\\s+[Aa]t\\s+([^,]+,)?(.+?):(\\s+line\\s+)?(\\d+)(\\s+char:\\d+)?$' -replace '\\\\', '\')
 
-        t "Matches problem pattern with single error" {
-            $setup = {
+        t 'Matches problem pattern with single error' {
+            $sb = {
                 $env:TERM_PROGRAM = 'vscode'
                 $psEditor = $null
-            }
 
-            $sb = {
-                Describe 'VSCode Output Test' {
-                    It 'Single error' {
-                        1 | Should -Be 2
+                $container = New-PesterContainer -ScriptBlock {
+                    Describe 'VSCode Output Test' {
+                        It 'Single error' {
+                            1 | Should -Be 2
+                        }
                     }
                 }
+                Invoke-Pester -Container $container
             }
 
-            $output = Invoke-PesterInProcess -ScriptBlock $sb -Setup $setup
+            $output = Invoke-InNewProcess -ScriptBlock $sb
 
             $hostSingleError = $output | Select-String -Pattern 'Single error' -Context 0, 1
             $hostSingleError.Line -match $titlePattern | Verify-True
             $hostSingleError.Context.PostContext[0] -match $atPattern | Verify-True
         }
 
-        t "Matches problem pattern with multiple errors" {
-            $setup = {
+        t 'Matches problem pattern with multiple errors' {
+            $sb = {
                 $env:TERM_PROGRAM = 'vscode'
                 $psEditor = $null
 
                 $PesterPreference = [PesterConfiguration]::Default
                 $PesterPreference.Should.ErrorAction = 'Continue'
-            }
 
-            $sb = {
-                Describe 'VSCode Output Test' {
-                    It 'Multiple errors' {
-                        1 | Should -Be 2
-                        1 | Should -Be 3
+                $container = New-PesterContainer -ScriptBlock {
+                    Describe 'VSCode Output Test' {
+                        It 'Multiple errors' {
+                            1 | Should -Be 2
+                            1 | Should -Be 3
+                        }
                     }
                 }
+                Invoke-Pester -Container $container
             }
 
-            $output = Invoke-PesterInProcess $sb -Setup $setup
+            $output = Invoke-InNewProcess $sb
 
             $hostMultipleErrors = $output | Select-String -Pattern 'Multiple errors' -Context 0, 1
             $hostMultipleErrors.Count | Verify-Equal 2
@@ -102,52 +100,54 @@ i -PassThru:$PassThru {
         }
     }
 
-    b "Output for nested blocks" {
-        t "All describes and contexts are output in Detailed mode" {
+    b 'Output for nested blocks' {
+        t 'All describes and contexts are output in Detailed mode' {
             # we postpone output of Describe and Context till we expand the name
             # so without walking up the stack we don't output them automatically
             # https://github.com/pester/Pester/issues/1716
 
             $sb = {
-                Describe "d1" {
-                    Context "c1" {
-                        It "i1" {
-                            1 | Should -Be 1
-                        }
-
-                        It "i2" {
-                            1 | Should -Be 1
-                        }
-                    }
-
-                    Context "c2" {
-                        It "i3" {
-                            1 | Should -Be 1
-                        }
-                    }
-                }
-
-                Describe "d failing" {
-                    # this failed but we should still see "c failing" and "d failing" on the screen
-                    Context "c failing" {
-                        BeforeAll { throw }
-
-                        It "i3" {
-                            1 | Should -Be 1
-                        }
-                    }
-                }
-            }
-
-            $setup = {
                 $PesterPreference = [PesterConfiguration]::Default
                 $PesterPreference.Output.Verbosity = 'Detailed'
                 $PesterPreference.Output.CIFormat = 'None'
                 $PesterPreference.Output.UseANSI = $false
+
+                $container = New-PesterContainer -ScriptBlock {
+                    Describe 'd1' {
+                        Context 'c1' {
+                            It 'i1' {
+                                1 | Should -Be 1
+                            }
+
+                            It 'i2' {
+                                1 | Should -Be 1
+                            }
+                        }
+
+                        Context 'c2' {
+                            It 'i3' {
+                                1 | Should -Be 1
+                            }
+                        }
+                    }
+
+                    Describe 'd failing' {
+                        # this failed but we should still see "c failing" and "d failing" on the screen
+                        Context 'c failing' {
+                            BeforeAll { throw }
+
+                            It 'i3' {
+                                1 | Should -Be 1
+                            }
+                        }
+                    }
+                }
+                Invoke-Pester -Container $container
             }
-            $output = Invoke-PesterInProcess $sb -Setup $setup
+
+            $output = Invoke-InNewProcess $sb
             # only print the relevant part of output
-            $null, $run = $output -join "`n" -split "Running tests."
+            $null, $run = $output -join "`n" -split 'Running tests.'
             $run | Write-Host
 
             $describe1 = $output | Select-String -Pattern 'Describing d1\s*$'
@@ -162,31 +162,33 @@ i -PassThru:$PassThru {
         }
     }
 
-    b "Output for data-driven blocks" {
-        t "Each block generated from dataset is output" {
+    b 'Output for data-driven blocks' {
+        t 'Each block generated from dataset is output' {
             # we incorrectly shared reference to the same framework data hashtable
             # so we only output the first context / describe, this test ensures each one is output
             # https://github.com/pester/Pester/issues/1759
 
             $sb = {
-                Describe "d1 <value>" -ForEach @(
-                    @{ Value = "abc" }
-                    @{ Value = "def" }
-                ) {
-                    It "i1" {
-                        1 | Should -Be 1
-                    }
-                }
-            }
-
-            $setup = {
                 $PesterPreference = [PesterConfiguration]::Default
                 $PesterPreference.Output.Verbosity = 'Detailed'
                 $PesterPreference.Output.UseANSI = $false
+
+                $container = New-PesterContainer -ScriptBlock {
+                    Describe 'd1 <value>' -ForEach @(
+                        @{ Value = 'abc' }
+                        @{ Value = 'def' }
+                    ) {
+                        It 'i1' {
+                            1 | Should -Be 1
+                        }
+                    }
+                }
+                Invoke-Pester -Container $container
             }
-            $output = Invoke-PesterInProcess $sb -Setup $setup
+
+            $output = Invoke-InNewProcess $sb
             # only print the relevant part of output
-            $null, $run = $output -join "`n" -split "Running tests."
+            $null, $run = $output -join "`n" -split 'Running tests.'
             $run | Write-Host
 
             $describe1 = $output | Select-String -Pattern 'Describing d1 abc\s*$'
