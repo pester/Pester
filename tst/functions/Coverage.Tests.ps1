61eefc143713679b25162fadf297b8f2071b6239
Joe Skeen <joeskeen@outlook.com>
2023-02-10 12:44:10 -0700

    Fix windows paths

--- tst/functions/Coverage.Tests.ps1
+++ tst/functions/Coverage.Tests.ps1
@@ -469,8 +469,7 @@ InPesterModuleScope {
                 [String]$coberturaReportXml = Get-CoberturaReportXml -CommandCoverage $breakpoints -TotalMilliseconds 10000 -CoverageReport $coverageReport
                 $coberturaReportXml = $coberturaReportXml -replace 'timestamp="[0-9]*"', 'timestamp=""'
                 $coberturaReportXml = $coberturaReportXml -replace "$([System.Environment]::NewLine)", ''
-                $coberturaReportXml = $coberturaReportXml -replace "$(Split-Path -Path $rootSubFolder)", 'CommonRoot'
-                $coberturaReportXml = $coberturaReportXml.Replace($root.Replace('\', '/'), '')
+                $coberturaReportXml = $coberturaReportXml.Replace($root.Replace('\', '/'), 'CommonRoot')
                 (Clear-WhiteSpace $coberturaReportXml) | Should -Be (Clear-WhiteSpace '
                 <?xml version="1.0" encoding="utf-8"?>
                 <!DOCTYPE coverage SYSTEM "http://cobertura.sourceforge.net/xml/coverage-04.dtd"[]>
