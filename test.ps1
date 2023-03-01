68fc80591ec624f0ccad71522433df664777beb0
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2023-03-01 18:35:31 +0000

    Avoid unnecessary builds in tests.ps1
    Brain fart in #2200.
    We already build beforing invoke the P-tests.

--- test.ps1
+++ test.ps1
@@ -62,8 +62,8 @@ Get-Module Pester | Remove-Module
 
 if (-not $SkipPTests) {
     $result = @(Get-ChildItem $PSScriptRoot/tst/*.ts.ps1 -Recurse |
-            foreach {
-                $r = & $_.FullName -PassThru -NoBuild:$NoBuild
+            ForEach-Object {
+                $r = & $_.FullName -PassThru -NoBuild:$true
                 if ($r.Failed -gt 0) {
                     [PSCustomObject]@{
                         FullName = $_.FullName
