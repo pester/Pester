6edf367e2a425c7894768dcf1ccfae02a9ebcf63
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2023-05-18 13:14:05 +0000

    Fix -DefaultValue for alias and throw on binary

--- src/functions/assertions/HaveParameter.ps1
+++ src/functions/assertions/HaveParameter.ps1
@@ -1,4 +1,4 @@
-function Should-HaveParameter (
+﻿function Should-HaveParameter (
     $ActualValue,
     [String] $ParameterName,
     $Type,
@@ -41,50 +41,47 @@ function Should-HaveParameter (
             [Parameter(Mandatory = $true)]
             [Management.Automation.CommandInfo]$Command
         )
-        <#
-        .SYNOPSIS
-            Use AST to get information about the parameter block of a command
-        .DESCRIPTION
-            In order to get information about the parameter block of a command,
-            several tools can be used (Get-Command, AST, etc).
-            In order to get the default value of a parameter, AST is the easiest
-            way to go
-        .NOTES
-            Author: Brian West
-        #>
 
-        # Find parameters
+        # Resolve alias to the actual command so we can access scriptblock
+        if ($Command -is [System.Management.Automation.AliasInfo] -and $Command.ResolvedCommand) {
+            $Command = $Command.ResolvedCommand
+        }
+
         $ast = $Command.ScriptBlock.Ast
 
-        if ($null -ne $ast) {
-            if ($null -ne $ast.Parameters) {
-                $parameters = $ast.Parameters
-            }
-            elseif ($null -ne $ast.Body.ParamBlock) {
-                $parameters = $ast.Body.ParamBlock.Parameters
-            }
-            else {
-                return
+        if ($null -eq $ast) {
+            # Ast is unavailable, ex. for a binary cmdlet
+            throw [ArgumentException]'Using -DefaultValue is only supported for scripts and functions.'
+        }
+
+        if ($null -ne $ast.Parameters) {
+            $parameters = $ast.Parameters
+        }
+        elseif ($null -ne $ast.Body.ParamBlock) {
+            $parameters = $ast.Body.ParamBlock.Parameters
+        }
+        else {
+            return
+        }
+
+        foreach ($parameter in $parameters) {
+            $paramInfo = [PSCustomObject] @{
+                Name             = $parameter.Name.VariablePath.UserPath
+                Type             = "[$($parameter.StaticType.Name.ToLower())]"
+                DefaultValue     = $null
+                DefaultValueType = $parameter.StaticType.Name
             }
 
-            foreach ($parameter in $parameters) {
-                $paramInfo = & $SafeCommands['New-Object'] PSObject -Property @{
-                    Name             = $parameter.Name.VariablePath.UserPath
-                    DefaultValueType = $parameter.StaticType.Name
-                    Type             = "[$($parameter.StaticType.Name.ToLower())]"
-                } | & $SafeCommands['Select-Object'] Name, Type, DefaultValue, DefaultValueType
-
-                if ($null -ne $parameter.DefaultValue) {
-                    if ($parameter.DefaultValue.PSObject.Properties['Value']) {
-                        $paramInfo.DefaultValue = $parameter.DefaultValue.Value
-                    }
-                    else {
-                        $paramInfo.DefaultValue = $parameter.DefaultValue.Extent.Text
-                    }
+            if ($null -ne $parameter.DefaultValue) {
+                if ($parameter.DefaultValue.PSObject.Properties['Value']) {
+                    $paramInfo.DefaultValue = $parameter.DefaultValue.Value
                 }
-
-                $paramInfo
+                else {
+                    $paramInfo.DefaultValue = $parameter.DefaultValue.Extent.Text
+                }
             }
+
+            $paramInfo
         }
     }
 
