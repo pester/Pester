c156209238ddc623938bbd0bee8d9295539d8cd7
Jakub Jareš <me@jakubjares.com>
2024-04-10 23:13:04 +0200

    Format because

--- src/functions/Set-ItResult.ps1
+++ src/functions/Set-ItResult.ps1
@@ -94,7 +94,7 @@
     }
 
     if ($Because) {
-        [String]$message += ", because $Because"
+        [String]$message += ", because $(Format-Because $Because)"
     }
 
     throw [Pester.Factory]::CreateErrorRecord(
