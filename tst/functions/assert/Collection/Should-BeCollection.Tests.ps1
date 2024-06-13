131539c4b6a9da636fdc6c49ab89d02039d37dfe
Jakub Jareš <me@jakubjares.com>
2024-06-13 21:06:05 +0200

    wip

--- tst/functions/assert/Collection/Should-BeCollection.Tests.ps1
+++ tst/functions/assert/Collection/Should-BeCollection.Tests.ps1
@@ -1,36 +1,37 @@
 ﻿Set-StrictMode -Version Latest
 
-# TODO: Implement the Should-BeCollection tests, I just don't want to remove it from the current PR just to put it back afterwards.
-return
+Describe "Should-BeCollection" {
+    It "Passes when collections have the same count and items" -ForEach @(
+        @{ Actual = @(1); Expected = @(1) }
+        @{ Actual = @(1, 2); Expected = @(1, 2) }
+        @{ Actual = @(1..3); Expected = @(1..3) }
+    ) {
+        $actual | Should-BeCollection $expected
+    }
 
-InPesterModuleScope {
-    Describe "Should-BeCollection" {
-        It "Passes when collections have the same count and items" -ForEach @(
-            @{ Actual = @(1); Expected = @(1) }
-            @{ Actual = @(1, 2); Expected = @(1, 2) }
-        ) {
-            $actual | Should-BeCollection $expected
-        }
+    It "Fails when collections don't have the same count" -ForEach @(
+        @{ Actual = @(1); Expected = @(1, 2) }
+        @{ Actual = @(1, 2); Expected = @(1) }
+    ) {
+        $err = { $actual | Should-BeCollection $expected } | Verify-AssertionFailed
+        $err.Exception.Message | Verify-Equal "Expected [collection] @($($expected -join ",")) to be equal to [collection] @($($actual -join ",")), but they had a different count of items."
+    }
 
-        It "Fails when collections don't have the same count" -ForEach @(
-            @{ Actual = @(1); Expected = @(1, 2) }
-            @{ Actual = @(1, 2); Expected = @(1) }
-        ) {
-            $err = { $actual | Should-BeCollection $expected } | Verify-AssertionFailed
-            $err.Exception.Message | Verify-Equal "Expected int '1' to be present in collection '5', but it was not there."
-        }
+    It "Fails when collections don't have the same items" -ForEach @(
+        @{ Actual = @(1, 3); Expected = @(1, 2); DifferenceIndex = @(1) }
+        @{ Actual = @(1, 2); Expected = @(2, 3); DifferenceIndex = @(0) }
+        @{ Actual = @(1, 2, 3); Expected = @(2, 3, 5); DifferenceIndex = @(0, 1, 2) }
+    ) {
+        $err = { $actual | Should-BeCollection $expected } | Verify-AssertionFailed
+        $err.Exception.Message | Verify-Equal "Expected [collection] @($($expected -join ",")) to be equal to [collection] @($($actual -join ",")), but they have different items."
+    }
 
-        # It "Passes when collection of multiple items contains the expected item" {
-        #     @(1,2,3) | Assert-Contain 1
-        # }
-
-        # It "Fails when collection of multiple items does not contain the expected item" {
-        #     $err = { @(5,6,7) | Assert-Contain 1 } | Verify-AssertionFailed
-        #     $err.Exception.Message | Verify-Equal "Expected int '1' to be present in collection '5, 6, 7', but it was not there."
-        # }
-
-        #  It "Can be called with positional parameters" {
-        #     { Assert-Contain 1 3,4,5 } | Verify-AssertionFailed
-        # }
+    It "Fails when collections don't have the same items" -ForEach @(
+        @{ Actual = @(1, 3); Expected = @(1, 2); DifferenceIndex = @(1) }
+        @{ Actual = @(1, 2); Expected = @(2, 3); DifferenceIndex = @(0) }
+        @{ Actual = @(1, 2, 3, 4, 5); Expected = @(5, 6, 7, 8, 9); DifferenceIndex = @(0, 1, 2) }
+    ) {
+        $err = { $actual | Should-BeCollection $expected } | Verify-AssertionFailed
+        $err.Exception.Message | Verify-Equal "Expected [collection] @($($expected -join ",")) to be equal to [collection] @($($actual -join ",")), but they don't have the same number of items."
     }
 }
