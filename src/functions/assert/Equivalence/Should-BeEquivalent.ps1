6c51746c60f628c8c9f622142658754682928ec0
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2024-06-13 21:54:42 +0000

    Only check if parameter is missing regardless of null
    Nested Compare-* calls got $null

--- src/functions/assert/Equivalence/Should-BeEquivalent.ps1
+++ src/functions/assert/Equivalence/Should-BeEquivalent.ps1
@@ -530,8 +530,8 @@ function Compare-Equivalent {
         $Options
     )
 
-    if ($null -eq $Options) {
-        throw [System.ArgumentNullException]::new('Options', 'Options must be defined and not null. If you see this and you are not developing Pester, please file issue at https://github.com/pester/Pester/issues')
+    if (-not $PSBoundParameters.ContainsKey('Options')) {
+        throw [System.ArgumentException]::new('-Options must be provided. If you see this and you are not developing Pester, please file issue at https://github.com/pester/Pester/issues','Options')
     }
 
     if ($null -ne $Options.ExcludedPaths -and $Options.ExcludedPaths -contains $Path) {
