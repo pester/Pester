0324d489eba434bd1a2e3bb64e9263593f7fda33
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2024-06-13 20:56:03 +0000

    Throw on missing Options-parameter in Compare-Equivalent

--- src/functions/assert/Equivalence/Should-BeEquivalent.ps1
+++ src/functions/assert/Equivalence/Should-BeEquivalent.ps1
@@ -1,4 +1,4 @@
-﻿function Test-Same ($Expected, $Actual) {
+function Test-Same ($Expected, $Actual) {
     [object]::ReferenceEquals($Expected, $Actual)
 }
 
@@ -527,11 +527,12 @@ function Compare-Equivalent {
         $Actual,
         $Expected,
         $Path,
-        $Options = (& {
-                & $SafeCommands['Write-Warning'] "Getting default equivalency options, this should never be seen. If you see this and you are not developing Pester, please file issue at https://github.com/pester/Pester/issues"
-                Get-EquivalencyOption
-            })
+        $Options
     )
+
+    if ($null -eq $Options) {
+        throw [System.ArgumentNullException]::new('Options', 'Options must be defined and not null. If you see this and you are not developing Pester, please file issue at https://github.com/pester/Pester/issues')
+    }
 
     if ($null -ne $Options.ExludedPaths -and $Options.ExcludedPaths -contains $Path) {
         Write-EquivalenceResult -Skip "Current path '$Path' is excluded from the comparison."
