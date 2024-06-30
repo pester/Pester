d1e81ff31175701900f9fc1400d9bc8c3fc14d7b
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2024-06-30 19:20:54 +0000

    Remove flattenBlock
    Not used and covered by Fold-Block -OnBlock ...

--- src/Pester.Runtime.ps1
+++ src/Pester.Runtime.ps1
@@ -2358,18 +2358,6 @@ function View-Flat {
     }
 }
 
-function flattenBlock ($Block, $Accumulator) {
-    $Accumulator.Add($Block)
-    if ($Block.Blocks.Count -eq 0) {
-        return $Accumulator
-    }
-
-    foreach ($bl in $Block.Blocks) {
-        flattenBlock -Block $bl -Accumulator $Accumulator
-    }
-    $Accumulator
-}
-
 function New-FilterObject {
     [CmdletBinding()]
     param (
