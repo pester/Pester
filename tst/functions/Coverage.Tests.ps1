ce55728e2c68b6eb27b1dbf98a66d6e6e85a9f61
Jakub Jareš <me@jakubjares.com>
2024-11-17 16:07:45 +0100

    bsclass

--- tst/functions/Coverage.Tests.ps1
+++ tst/functions/Coverage.Tests.ps1
@@ -97,7 +97,7 @@ InPesterModuleScope {
                 #{
                     function MyClass
                     {
-                        MyBaseClass; 'I am the constructor.'
+                        MyBaseClass # calls the base constructor
                     }
 
                     function MethodOne
