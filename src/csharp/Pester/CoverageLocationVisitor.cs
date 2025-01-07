79e6f00523a2ebc264c6937602aab2648bbd37c0
Frode Flaten <3436158+fflaten@users.noreply.github.com>
2025-01-07 20:22:55 +0000

    Cleanup usings

--- src/csharp/Pester/CoverageLocationVisitor.cs
+++ src/csharp/Pester/CoverageLocationVisitor.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
-using System.Management.Automation;
 using System.Management.Automation.Language;
 
 namespace Pester
