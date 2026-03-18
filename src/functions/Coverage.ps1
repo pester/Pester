12a8d44179b79b0248e01d3445bc5a14fbca90b7
nohwnd <me@jakubjares.com>
2026-03-18 21:44:05 +0100

    slash

--- src/functions/Coverage.ps1
+++ src/functions/Coverage.ps1
@@ -731,6 +731,14 @@ function Get-ReportRoot {
 
 function Get-RelativePath {
     param ( [string] $Path, [string] $RelativeTo )
+    if ([System.IO.Path]::DirectorySeparatorChar -eq '/') {
+        # On Unix, also trim the alternate directory separator char, since both are used as separators
+        $RelativeTo = $RelativeTo.Replace('\', '/').TrimEnd('/')
+    }
+    else {
+        $RelativeTo = $RelativeTo.Replace('\').TrimEnd('\')
+    }
+
     return $Path -replace "^$([regex]::Escape("$RelativeTo$([System.IO.Path]::DirectorySeparatorChar)"))?"
 }
 
