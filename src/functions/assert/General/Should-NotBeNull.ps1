67422c17b16549af8c375a3075e2ed13cf1c6974
Jakub Jareš <me@jakubjares.com>
2026-07-31 17:23:50 +0200

    Fix #2933: make Actual parameter position consistent across the new assertions

--- src/functions/assert/General/Should-NotBeNull.ps1
+++ src/functions/assert/General/Should-NotBeNull.ps1
@@ -34,7 +34,7 @@
     [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseProcessBlockForPipelineCommand', '')]
     [CmdletBinding()]
     param (
-        [Parameter(Position = 1, ValueFromPipeline = $true)]
+        [Parameter(Position = 0, ValueFromPipeline = $true)]
         $Actual,
         [String]$Because
     )
